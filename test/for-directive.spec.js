a02af5293f953226e2b6b138d12ff98b93c3c233
erik <erik168@163.com>
2016-07-05 21:20:53 +0800

    for-directive specs

--- test/for-directive.spec.js
+++ test/for-directive.spec.js
@@ -2,7 +2,7 @@ describe("ForDirective", function () {
 
     it("render list, data fill before attach", function () {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -16,7 +16,7 @@ describe("ForDirective", function () {
 
         var lis = wrap.getElementsByTagName('li');
 
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
         expect(lis[2].getAttribute('title')).toBe('varsha');
         expect(lis[2].innerHTML.indexOf('varsha - wangshuonpu@163.com')).toBe(0);
         expect(lis[1].getAttribute('title')).toBe('errorrik');
@@ -26,11 +26,29 @@ describe("ForDirective", function () {
         document.body.removeChild(wrap);
     });
 
+    it("render list, no data", function () {
+        var MyComponent = sanVM.Component({
+            template: '<ul><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+        });
+        var myComponent = new MyComponent();
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var lis = wrap.getElementsByTagName('li');
+
+        expect(lis.length).toBe(0);
+
+        myComponent.dispose();
+        document.body.removeChild(wrap);
+    });
+
 
 
     it("data push after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -43,7 +61,7 @@ describe("ForDirective", function () {
         myComponent.attach(wrap);
 
         var lis = wrap.getElementsByTagName('li');
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
 
         myComponent.data.push('persons',
             {name: 'otakustay', email: 'otakustay@gmail.com'}
@@ -51,7 +69,7 @@ describe("ForDirective", function () {
 
         sanVM.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
-            expect(lis.length).toBe(4);
+            expect(lis.length).toBe(5);
             expect(lis[3].getAttribute('title')).toBe('otakustay');
             expect(lis[3].innerHTML.indexOf('otakustay - otakustay@gmail.com')).toBe(0);
 
@@ -63,7 +81,7 @@ describe("ForDirective", function () {
 
     it("data pop after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -76,13 +94,13 @@ describe("ForDirective", function () {
         myComponent.attach(wrap);
 
         var lis = wrap.getElementsByTagName('li');
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
 
         myComponent.data.pop('persons');
 
         sanVM.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
-            expect(lis.length).toBe(2);
+            expect(lis.length).toBe(3);
             expect(lis[1].getAttribute('title')).toBe('errorrik');
             expect(lis[1].innerHTML.indexOf('errorrik - errorrik@gmail.com')).toBe(0);
 
@@ -94,7 +112,7 @@ describe("ForDirective", function () {
 
     it("data unshift after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -107,7 +125,7 @@ describe("ForDirective", function () {
         myComponent.attach(wrap);
 
         var lis = wrap.getElementsByTagName('li');
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
 
         myComponent.data.unshift('persons',
             {name: 'otakustay', email: 'otakustay@gmail.com'}
@@ -115,7 +133,7 @@ describe("ForDirective", function () {
 
         sanVM.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
-            expect(lis.length).toBe(4);
+            expect(lis.length).toBe(5);
             expect(lis[3].getAttribute('title')).toBe('varsha');
             expect(lis[3].innerHTML.indexOf('varsha - wangshuonpu@163.com')).toBe(0);
             expect(lis[1].getAttribute('title')).toBe('otakustay');
@@ -129,7 +147,7 @@ describe("ForDirective", function () {
 
     it("data shift after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -142,13 +160,13 @@ describe("ForDirective", function () {
         myComponent.attach(wrap);
 
         var lis = wrap.getElementsByTagName('li');
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
 
         myComponent.data.shift('persons');
 
         sanVM.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
-            expect(lis.length).toBe(2);
+            expect(lis.length).toBe(3);
             expect(lis[1].getAttribute('title')).toBe('varsha');
             expect(lis[1].innerHTML.indexOf('varsha - wangshuonpu@163.com')).toBe(0);
 
@@ -160,7 +178,7 @@ describe("ForDirective", function () {
 
     it("data set after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -173,13 +191,13 @@ describe("ForDirective", function () {
         myComponent.attach(wrap);
 
         var lis = wrap.getElementsByTagName('li');
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
 
         myComponent.data.set('persons[0]', {name: 'erik', email: 'erik168@163.com'});
 
         sanVM.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
-            expect(lis.length).toBe(3);
+            expect(lis.length).toBe(4);
             expect(lis[1].getAttribute('title')).toBe('erik');
             expect(lis[1].innerHTML.indexOf('erik - erik168@163.com')).toBe(0);
 
@@ -191,7 +209,7 @@ describe("ForDirective", function () {
 
     it("data item set after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -204,13 +222,13 @@ describe("ForDirective", function () {
         myComponent.attach(wrap);
 
         var lis = wrap.getElementsByTagName('li');
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
 
         myComponent.data.set('persons[0].name', 'erik');
 
         sanVM.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
-            expect(lis.length).toBe(3);
+            expect(lis.length).toBe(4);
             expect(lis[1].getAttribute('title')).toBe('erik');
             expect(lis[1].innerHTML.indexOf('erik - errorrik@gmail.com')).toBe(0);
 
@@ -222,7 +240,7 @@ describe("ForDirective", function () {
 
     it("data set after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -235,7 +253,7 @@ describe("ForDirective", function () {
         myComponent.attach(wrap);
 
         var lis = wrap.getElementsByTagName('li');
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
 
         myComponent.data.set('persons', [
             {name: 'otakustay', email: 'otakustay@gmail.com'}
@@ -243,7 +261,7 @@ describe("ForDirective", function () {
 
         sanVM.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
-            expect(lis.length).toBe(2);
+            expect(lis.length).toBe(3);
             expect(lis[1].getAttribute('title')).toBe('otakustay');
             expect(lis[1].innerHTML.indexOf('otakustay - otakustay@gmail.com')).toBe(0);
 
@@ -255,7 +273,7 @@ describe("ForDirective", function () {
 
     it("data set after attach", function (done) {
         var MyComponent = sanVM.Component({
-            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}} in {{org}}</li></ul>'
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}} in {{org}}</li><li>name - email</li></ul>'
         });
         var myComponent = new MyComponent();
         myComponent.set('persons', [
@@ -270,7 +288,7 @@ describe("ForDirective", function () {
 
         var lis = wrap.getElementsByTagName('li');
 
-        expect(lis.length).toBe(3);
+        expect(lis.length).toBe(4);
         expect(lis[1].getAttribute('title')).toBe('errorrik');
         expect(lis[1].innerHTML.indexOf('errorrik - errorrik@gmail.com in efe')).toBe(0);
 
@@ -286,5 +304,108 @@ describe("ForDirective", function () {
         });
     });
 
+    it("no data before attach, data set after attach", function (done) {
+        var MyComponent = sanVM.Component({
+            template: '<ul><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}</li></ul>'
+        });
+        var myComponent = new MyComponent();
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var lis = wrap.getElementsByTagName('li');
+        expect(lis.length).toBe(0);
+
+        myComponent.data.set('persons', [
+            {name: 'otakustay', email: 'otakustay@gmail.com'}
+        ]);
+
+        sanVM.nextTick(function () {
+            var lis = wrap.getElementsByTagName('li');
+            expect(lis.length).toBe(1);
+            expect(lis[0].getAttribute('title')).toBe('otakustay');
+            expect(lis[0].innerHTML.indexOf('otakustay - otakustay@gmail.com')).toBe(0);
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        });
+    });
+
+    it("nested, data fill before attach", function () {
+        var MyComponent = sanVM.Component({
+            template: '<ul><li>name - email</li><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}<b san-for="tel in p.tels">{{tel}}</b></li></ul>'
+        });
+        var myComponent = new MyComponent();
+        myComponent.set('persons', [
+            {name: 'errorrik', email: 'errorrik@gmail.com', tels: ['12345678', '87654321']},
+            {name: 'varsha', email: 'wangshuonpu@163.com', tels: ['23456789', '98765432']}
+        ]);
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var lis = wrap.getElementsByTagName('li');
+
+        expect(lis.length).toBe(3);
+
+        expect(lis[2].getAttribute('title')).toBe('varsha');
+        expect(lis[2].innerHTML.indexOf('varsha - wangshuonpu@163.com')).toBe(0);
+        var lis2bs = lis[2].getElementsByTagName('b');
+        expect(lis2bs[0].innerHTML.indexOf('23456789')).toBe(0);
+        expect(lis2bs[1].innerHTML.indexOf('98765432')).toBe(0);
+
+        expect(lis[1].getAttribute('title')).toBe('errorrik');
+        expect(lis[1].innerHTML.indexOf('errorrik - errorrik@gmail.com')).toBe(0);
+        var lis1bs = lis[1].getElementsByTagName('b');
+        expect(lis1bs[0].innerHTML.indexOf('12345678')).toBe(0);
+        expect(lis1bs[1].innerHTML.indexOf('87654321')).toBe(0);
+
+        myComponent.dispose();
+        document.body.removeChild(wrap);
+    });
+
+    it("nested, data fill after attach", function (done) {
+        var MyComponent = sanVM.Component({
+            template: '<ul><li san-for="p,i in persons" title="{{p.name}}">{{p.name}} - {{p.email}}<b san-for="tel in p.tels">{{tel}}</b></li></ul>'
+        });
+        var myComponent = new MyComponent();
+
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var lis = wrap.getElementsByTagName('li');
+        expect(lis.length).toBe(0);
+
+        myComponent.data.set('persons', [
+            {name: 'errorrik', email: 'errorrik@gmail.com', tels: ['12345678', '87654321']},
+            {name: 'varsha', email: 'wangshuonpu@163.com', tels: ['23456789', '98765432']}
+        ]);
+
+        sanVM.nextTick(function () {
+            var lis = wrap.getElementsByTagName('li');
+
+            expect(lis[1].getAttribute('title')).toBe('varsha');
+            expect(lis[1].innerHTML.indexOf('varsha - wangshuonpu@163.com')).toBe(0);
+            var lis2bs = lis[1].getElementsByTagName('b');
+            expect(lis2bs[0].innerHTML.indexOf('23456789')).toBe(0);
+            expect(lis2bs[1].innerHTML.indexOf('98765432')).toBe(0);
+
+            expect(lis[0].getAttribute('title')).toBe('errorrik');
+            expect(lis[0].innerHTML.indexOf('errorrik - errorrik@gmail.com')).toBe(0);
+            var lis1bs = lis[0].getElementsByTagName('b');
+            expect(lis1bs[0].innerHTML.indexOf('12345678')).toBe(0);
+            expect(lis1bs[1].innerHTML.indexOf('87654321')).toBe(0);
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+
+            done();
+        });
+
+    });
 
 });
