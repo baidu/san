9e01306b567fe498d2027937573dd99f75a81895
erik <erik168@163.com>
2016-12-26 16:58:13 +0800

    3.0.0-rc.4

--- src/main.js
+++ src/main.js
@@ -4526,7 +4526,7 @@
      * @param {string} name 名称
      * @param {function(*, ...*):*} filter 过滤函数
      */
-    san.version = '3.0.0-rc.3';
+    san.version = '3.0.0-rc.4';
 
     /**
      * 根据 DOM id 获取内部元素对象
