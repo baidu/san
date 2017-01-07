447aafaa9e3bca5549a40f067f7aea2f0d57a69b
erik <erik168@163.com>
2017-01-07 11:35:53 +0800

    use san-router

--- example/todos-esnext/src/main.js
+++ example/todos-esnext/src/main.js
@@ -5,18 +5,25 @@ require('font-awesome/css/font-awesome.min.css')
 require('./main.css')
 
 // route
-import router from './router'
 import List from './components/todo/List'
 import Form from './components/todo/Form'
 import AddCategory from './components/category/Add'
 import EditCategory from './components/category/Edit'
 
-router.route('/', List)
-router.route(/^\/todos\/category\/([0-9]+)$/, List)
-router.route(/^\/add$/, Form)
-router.route(/^\/edit\/([0-9]+)$/, Form)
-router.route(/^\/category\/add$/, AddCategory)
-router.route(/^\/category\/edit$/, EditCategory)
+import {router} from 'san-router'
+
+router.add({rule: '/', Component: List, target: '#app'});
+router.add({rule: '/todos/category/:category', Component: List, target: '#app'});
+router.add({rule: '/add', Component: Form, target: '#app'});
+router.add({rule: '/edit/:id', Component: Form, target: '#app'});
+
+
+// router.route('/', List)
+// router.route(/^\/todos\/category\/([0-9]+)$/, List)
+// router.route(/^\/add$/, Form)
+// router.route(/^\/edit\/([0-9]+)$/, Form)
+// router.route(/^\/category\/add$/, AddCategory)
+// router.route(/^\/category\/edit$/, EditCategory)
 
 // start
 router.start()
