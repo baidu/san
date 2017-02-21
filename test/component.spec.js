dea030f83d9fd152c0b2906cc964804c20115267
erik <erik168@163.com>
2017-02-21 22:46:17 +0800

    add watch specs

--- test/component.spec.js
+++ test/component.spec.js
@@ -723,4 +723,104 @@ describe("Component", function () {
         detect();
     });
 
+    it("watch simple data item", function (done) {
+        var MyComponent = san.defineComponent({
+            template: '<a><span title="{{email}}">{{name}}</span></a>'
+        });
+        var myComponent = new MyComponent();
+        myComponent.data.set('email', 'errorrik@gmail.com');
+        myComponent.data.set('name', 'errorrik');
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var watchTriggerTimes = 0;
+        myComponent.watch('email', function (value) {
+            expect(value).toBe('erik168@163.com');
+            expect(this.data.get('email')).toBe(value);
+            watchTriggerTimes++;
+        });
+
+        myComponent.data.set('email', 'erik168@163.com');
+        myComponent.data.set('name', 'erik');
+        expect(watchTriggerTimes).toBe(1);
+
+        var span = wrap.getElementsByTagName('span')[0];
+        expect(span.title).toBe('errorrik@gmail.com');
+
+        san.nextTick(function () {
+            expect(watchTriggerTimes).toBe(1);
+
+            var span = wrap.getElementsByTagName('span')[0];
+            expect(span.title).toBe('erik168@163.com');
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        })
+
+    });
+
+    it("watch property accessor", function (done) {
+        var MyComponent = san.defineComponent({
+            template: '<a><span title="{{projects[0].author.email}}">{{projects[0].author.email}}</span></a>',
+
+            initData: function () {
+                return {
+                    projects: [
+                        {
+                            name: 'etpl',
+                            author: {
+                                email: 'errorrik@gmail.com',
+                                name: 'errorrik'
+                            }
+                        }
+                    ]
+
+                };
+            }
+        });
+        var myComponent = new MyComponent();
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var watchTriggerTimes = 0;
+        myComponent.watch('projects[0].author', function (value) {
+            expect(value.email).toBe('erik168@163.com');
+            expect(this.data.get('projects[0].author.email')).toBe(value.email);
+            watchTriggerTimes++;
+        });
+
+        var emailTriggerTimes = 0;
+        myComponent.watch('projects[0].author.email', function (value) {
+            expect(value).toBe('erik168@163.com');
+            expect(this.data.get('projects[0].author.email')).toBe(value);
+            emailTriggerTimes++;
+        });
+
+        myComponent.data.set('projects[0].author.email', 'erik168@163.com');
+        myComponent.data.set('projects[0].author.name', 'erik');
+        expect(watchTriggerTimes).toBe(2);
+        expect(emailTriggerTimes).toBe(1);
+
+        var span = wrap.getElementsByTagName('span')[0];
+        expect(span.title).toBe('errorrik@gmail.com');
+
+        san.nextTick(function () {
+            expect(watchTriggerTimes).toBe(2);
+            expect(emailTriggerTimes).toBe(1);
+
+            var span = wrap.getElementsByTagName('span')[0];
+            expect(span.title).toBe('erik168@163.com');
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        })
+
+    });
 });
+
