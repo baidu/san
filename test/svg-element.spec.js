fb07e933cc71e83f43f560ee32642e96b2e80a7d
menglingjun <menglingjun@baidu.com>
2017-03-29 22:21:47 +0800

    test svg

---
+++ test/svg-element.spec.js
@@ -0,0 +1,53 @@
+describe("SVG", function () {
+
+    var myComponent;
+    var wrap;
+
+    beforeEach(function(done) {
+
+        var MyComponent = san.defineComponent({
+            template: '<svg><rect width="{{size}}" height="{{size}}" /></svg>',
+            initData: function() {
+                return {
+                    size: 50
+                };
+            }
+        });
+
+        myComponent = new MyComponent();
+
+        wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        done();
+
+    });
+
+    afterEach(function() {
+        myComponent.dispose();
+        document.body.removeChild(wrap);
+    });
+
+    it("create element", function () {
+
+        myComponent.attach(wrap);
+
+        var rect = wrap.getElementsByTagName('rect')[0];
+        expect(+rect.getAttribute('width')).toBe(50);
+
+    });
+
+    it("setAttrbute", function (done) {
+
+        myComponent.attach(wrap);
+        var rect = wrap.getElementsByTagName('rect')[0];
+        var size = 100;
+        myComponent.data.set('size', size);
+
+        san.nextTick(function() {
+            expect(+rect.getAttribute('width')).toBe(size);
+            done();
+        });
+
+    });
+
+});
