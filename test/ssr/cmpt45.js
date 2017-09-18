689ddea2041a2bf1395939544a5bd754d44279a9
erik <erik168@163.com>
2017-09-18 21:19:53 +0800

    template error

--- test/ssr/cmpt45.js
+++ test/ssr/cmpt45.js
@@ -3,9 +3,9 @@ var san = require('../../dist/san.ssr');
 
 var MyComponent = san.defineComponent({
     template: '<div><span s-if="num > 10000" title="biiig">biiig</span>  \n'
-    + '<span s-elif="num > 1000" title="biig">biig</span>  \n' +
-    + '<span s-elif="num > 100" title="big">big</span>  \n' +
-    ' <b s-else title="small">small</b></div>'
+        + '<span s-elif="num > 1000" title="biig">biig</span>  \n'
+        + '<span s-elif="num > 100" title="big">big</span>  \n'
+        + ' <b s-else title="small">small</b></div>'
 });
 
 exports = module.exports = MyComponent;
