3826803b72f20d68114713106e8c6645e05271d1
erik <erik168@163.com>
2017-09-18 21:18:44 +0800

    spec template error

--- test/if-directive.spec.js
+++ test/if-directive.spec.js
@@ -307,9 +307,9 @@ describe("IfDirective", function () {
     it("multi elif", function (done) {
         var MyComponent = san.defineComponent({
             template: '<div><span s-if="num > 10000" title="biiig">biiig</span>  \n'
-            + '<span s-elif="num > 1000" title="biig">biig</span>  \n' +
-            + '<span s-elif="num > 100" title="big">big</span>  \n' +
-            ' <b s-else title="small">small</b></div>'
+            + '<span s-elif="num > 1000" title="biig">biig</span>  \n'
+            + '<span s-elif="num > 100" title="big">big</span>  \n'
+            + ' <b s-else title="small">small</b></div>'
         });
         var myComponent = new MyComponent({
             data: {
