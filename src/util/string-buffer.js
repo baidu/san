404f694d6ea2e246f57282dd29d48066b4f06ee6
erik <erik168@163.com>
2017-10-07 23:28:20 +0800

    dont use class

--- src/util/string-buffer.js
+++ src/util/string-buffer.js
@@ -13,40 +13,33 @@ var ie = require('../browser/ie');
  */
 var isCompatStringJoin = ie && ie < 8;
 
-
 /**
- * 写个用于跨平台提高性能的字符串连接类
+ * 写个用于跨平台提高性能的字符串连接
  * 万一不小心支持老式浏览器了呢
- *
- * @class
  */
-function StringBuffer() {
-    this.raw = isCompatStringJoin ? [] : '';
-    this.length = 0;
+
+function createStrBuffer() {
+    return {
+        raw: isCompatStringJoin ? [] : '',
+        length: 0
+    };
 }
 
-/**
- * 获取连接的字符串结果
- *
- * @return {string}
- */
-StringBuffer.prototype.toString = function () {
-    return isCompatStringJoin ? this.raw.join('') : this.raw;
-};
-
-/**
- * 增加字符串片段
- * 就不支持多参数，别问我为什么，这东西也不是给外部用的
- *
- * @param {string} source 字符串片段
- */
-StringBuffer.prototype.push = isCompatStringJoin
-    ? function (source) {
-        this.raw[this.length++] = source;
+var pushStrBuffer = isCompatStringJoin
+    ? function (buf, str) {
+        buf.raw[buf.length++] = str;
     }
-    : function (source) {
-        this.length++;
-        this.raw += source;
+    : function (buf, str) {
+        buf.raw += str;
     };
+
+
+function strBufferToStr(buf) {
+    if (isCompatStringJoin) {
+        return buf.raw.join('');
+    }
+
+    return buf.raw;
+}
 
 exports = module.exports = StringBuffer;
