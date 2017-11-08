94232404992d52b765f71db20c1c83ee5e9ac38d
kekee000 <kekee000@gmail.com>
2017-11-09 01:30:24 +0800

    support default event #114

--- test/element-event.spec.js
+++ test/element-event.spec.js
@@ -44,5 +44,41 @@ describe("Element-Event", function () {
 
     });
 
+    it("bind click fire default event", function (done) {
+        var clicked = 0;
+
+        var MyComponent = san.defineComponent({
+            template: '<a><span title="{{name}}" on-click="clicker()" style="color: red; cursor: pointer">please click here!</span><span title="{{name}}" on-click="clickerNoArgs" style="color: red; cursor: pointer">please click here!</span></a>',
+
+            clicker: function (event) {
+                expect(event.target || event.srcElement).toBe(span);
+                clicked += 1;
+            },
+            clickerNoArgs: function (event) {
+                expect(event.target || event.srcElement).toBe(spanNoArgs);
+                clicked += 1;
+            }
+        });
+        var myComponent = new MyComponent();
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+        var span = wrap.firstChild.firstChild;
+        var spanNoArgs = wrap.firstChild.lastChild;
+        function doneSpec() {
+            if (clicked == 2) {
+                done();
+                myComponent.dispose();
+                document.body.removeChild(wrap);
+                return;
+            }
+            setTimeout(doneSpec, 500);
+        }
+
+        triggerEvent('#' + span.id, 'click');
+        triggerEvent('#' + spanNoArgs.id, 'click');
+        doneSpec();
+    });
 
 });
