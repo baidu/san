25e40787e3e31becec17517b5244f88e64e147b9
erik <erik168@163.com>
2017-11-19 09:16:16 +0800

    #148 implement

---
+++ src/view/compile-component.js
@@ -0,0 +1,77 @@
+/**
+ * @file 编译组件类
+ * @author errorrik(errorrik@gmail.com)
+ */
+
+
+/**
+ * 编译组件类。预解析template和components
+ *
+ * @param {Function} ComponentClass 组件类
+ */
+function compileComponent(ComponentClass) {
+    var proto = ComponentClass.prototype;
+
+    // pre define components class
+    if (!proto.hasOwnProperty('_cmptReady')) {
+        proto.components =  ComponentClass.components || proto.components || {};
+        var components = proto.components;
+
+        for (var key in components) {
+            var componentClass = components[key];
+
+            if (typeof componentClass === 'object') {
+                components[key] = defineComponent(componentClass);
+            }
+            else if (componentClass === 'self') {
+                components[key] = ComponentClass;
+            }
+        }
+
+        proto._cmptReady = 1;
+    }
+
+
+    // pre compile template
+    if (!proto.hasOwnProperty('aNode')) {
+        proto.aNode = createANode();
+
+        var tpl = ComponentClass.template || proto.template;
+        if (tpl) {
+            var aNode = parseTemplate(tpl, {
+                trimWhitespace: proto.trimWhitespace || ComponentClass.trimWhitespace
+            });
+            var firstChild = aNode.children[0];
+
+            // #[begin] error
+            if (aNode.children.length !== 1 || firstChild.isText) {
+                throw new Error('[SAN FATAL] template must have a root element.');
+            }
+            // #[end]
+
+            proto.aNode = firstChild;
+            if (firstChild.tagName === 'template') {
+                firstChild.tagName = null;
+            }
+
+            firstChild.binds = new IndexedList();
+
+            each(componentPropExtra, function (extra) {
+                var prop = firstChild.props.get(extra.name);
+                if (prop) {
+                    prop.expr.segs.push(extra.expr.segs[0]);
+                    prop.expr.value = null;
+                    prop.attr = null;
+                }
+                else {
+                    firstChild.props.push({
+                        name: extra.name,
+                        expr: extra.expr
+                    });
+                }
+            });
+        }
+    }
+}
+
+exports = module.exports = compileComponent;
