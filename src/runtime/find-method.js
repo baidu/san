d68417b440560cf2eaab3da5faafdfff274c99d5
erik <erik168@163.com>
2017-11-30 20:18:23 +0800

    add comments

--- src/runtime/find-method.js
+++ src/runtime/find-method.js
@@ -1,8 +1,19 @@
-
+/**
+ * @file 在对象上使用accessor表达式查找方法
+ * @author errorrik(errorrik@gmail.com)
+ */
 
 var each = require('../util/each');
 var evalExpr = require('../runtime/eval-expr');
 
+/**
+ * 在对象上使用accessor表达式查找方法
+ * 
+ * @param {Object} source 源对象
+ * @param {Object} nameExpr 表达式
+ * @param {Data} data 所属数据环境
+ * @return {Function}
+ */
 function findMethod(source, nameExpr, data) {
     var method = source;
     each(nameExpr.paths, function (pathExpr) {
