fb39c4ff15e0b3739485a87f7cfe009798d55f1c
varsha <wangshuonpu@163.com>
2018-01-06 21:30:29 +0800

    add filter formatHour

--- example/todos-esnext/src/filters.js
+++ example/todos-esnext/src/filters.js
@@ -1,5 +1,12 @@
-import moment from 'moment'
+import moment from 'moment';
 
 export const formatDate = (value, format) => {
-    return moment(value).format(format)
-}
+    return value instanceof Date
+        ? moment(value).format(format)
+        : '';
+};
+
+export const formatHour = hour => {
+    hour = hour || 0;
+    return (hour % 12 || 12) + ':00 ' + ( hour < 12 ? 'am' : 'pm');
+};
