f3b5306ecb647f9bd524c980d4456fd87ee51789
houyu01 <houyu01@baidu.com>
2018-01-09 23:48:51 +0800

    fix error when using space around attribute's = operator

--- src/parser/parse-template.js
+++ src/parser/parse-template.js
@@ -46,7 +46,7 @@ function parseTemplate(source, options) {
     var walker = new Walker(source);
 
     var tagReg = /<(\/)?([a-z0-9-]+)\s*/ig;
-    var attrReg = /([-:0-9a-z\(\)\[\]]+)(=(['"])([^\3]*?)\3)?\s*/ig;
+    var attrReg = /([-:0-9a-z\(\)\[\]]+)(\s*=\s*(['"])([^\3]*?)\3)?\s*/ig;
 
     var tagMatch;
     var currentNode = rootNode;
