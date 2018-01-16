fa5212c3b6a8838ee47258305fcec757cd476c39
zhoumin09 <zhoumin09@baidu.com>
2018-01-16 19:10:29 +0800

    refactor: remove previous test components

--- test/ssr/cmpt27.js
+++
@@ -1,9 +0,0 @@
-// checkbox checked
-var san = require('../../dist/san.ssr');
-var MyComponent = san.defineComponent({
-    template: '<div>'
-        + '<input san-for="item in list" type="radio" value="{{item}}" checked="{=cValue=}">'
-        + '</div>'
-});
-
-exports = module.exports = MyComponent;
--- test/ssr/cmpt38.js
+++
@@ -1,8 +0,0 @@
-// complex structure in textnode
-var san = require('../../dist/san.ssr');
-
-var MyComponent = san.defineComponent({
-    template: '<a><span>aaa</span>hello {{name|raw}}!<b>bbb</b></a>'
-});
-
-exports = module.exports = MyComponent;
