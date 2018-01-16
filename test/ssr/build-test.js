baf269896440482f13b1448c828ffcb421acc93e
zhoumin09 <zhoumin09@baidu.com>
2018-01-16 14:46:55 +0800

    refactor: rewrite build-test

--- test/ssr/build-test.js
+++ test/ssr/build-test.js
@@ -1,379 +1,140 @@
+/**
+ * @file build test file
+ * @author zhoumin
+ */
+
+
 const san = require('../../dist/san.ssr');
 const fs = require('fs');
 const path = require('path');
 
-
-let componentDatas = [
-    {
-        email: 'errorrik@gmail.com',
-        name: 'errorrik'
-    },
-    {
-        email: 'errorrik@gmail.com',
-        name: 'errorrik'
-    },
-    {
-        jokeName: 'airike',
-        name: 'errorrik',
-        school: 'none',
-        company: 'bidu'
-    },
-    {
-        jokeName: 'airike',
-        name: 'errorrik',
-        school: 'none',
-        company: 'bidu'
-    },
-    {
-        company: 'bidu'
-    },
-    {
-        persons: []
-    },
-    {
-        persons: [
-            {name: 'errorrik', email: 'errorrik@gmail.com'},
-            {name: 'otakustay', email: 'otakustay@gmail.com'}
-        ]
-    },
-    {
-        persons: [
-            {name: 'errorrik', email: 'errorrik@gmail.com'},
-            {name: 'otakustay', email: 'otakustay@gmail.com'}
-        ]
-    },
-    {
-        persons: [
-            {name: 'errorrik', email: 'errorrik@gmail.com'},
-            {name: 'otakustay', email: 'otakustay@gmail.com'}
-        ]
-    },
-    {
-        cond: true,
-        name: 'errorrik'
-    },
-    {
-        cond: false,
-        name: 'errorrik'
-    },
-    {
-        'cond': false,
-        'name': 'errorrik',
-        name2: 'otakustay'
-    },
-    {
-        'cond': true,
-        'name': 'errorrik',
-        name2: 'otakustay'
-    },
-    {
-        tabText: 'tab',
-        text: 'one',
-        title: '1'
-    },
-    {
-        tabText: 'tab',
-        text: 'one',
-        title: '1',
-        tTitle: '5',
-        tText: 'five'
-    },
-    {
-        name: 'errorrik'
-    },
-    {
-        name: 'errorrik'
-    },
-    {
-        cond: true,
-        jokeName: 'airike',
-        name: 'errorrik',
-        school: 'none',
-        company: 'bidu'
-    },
-    {
-        cond: false,
-        jokeName: 'airike',
-        name: 'errorrik',
-        school: 'none',
-        company: 'bidu'
-    },
-    {
-        cond: true,
-        jokeName: 'airike',
-        name: 'errorrik',
-        school: 'none',
-        company: 'bidu'
-    },
-    {
-        list: [
-            {title: '1', text: 'one'},
-            {title: '2', text: 'two'}
-        ]
-    },
-    {
-        list: [
-            {title: '1', text: 'one'},
-            {title: '2', text: 'two'}
-        ]
-    },
-    {
-        cond: true,
-        persons: [
-            {
-                name: 'erik',
-                tels: [
-                    '12345678',
-                    '123456789',
-                ]
-            },
-            {
-                name: 'firede',
-                tels: [
-                    '2345678',
-                    '23456789',
-                ]
-            }
-        ]
-    },
-    {},
-    {distate: true},
-    {cValue: ['2', '3'], list: ['1', '2', '3']},
-    {cValue: '2', list: ['1', '2', '3']},
-    {
-        persons: ['errorrik', 'firede', 'otakustay'],
-        online: 'firede'
-    },
-    {
-        date: new Date(1983, 8, 3)
-    },
-    {},
-    {},
-    {distate: true},
-    {},
-    {html: 'aa<a>bbb</a>cc'},
-    {
-        title: '1',
-        text: 'one'
-    },
-    {
-        title: '1',
-        text: 'one'
-    },
-    {
-        title: '1'
-    },
-    {
-        name: 'er<u>erik</u>ik'
-    },
-    {
-        persons: ['errorrik', 'firede']
-    },
-    {
-        online: 'firede',
-        persons: ['errorrik', 'firede']
-    },
-    {},
-    {},
-    {
-        cond1: true,
-        cond2: true
-    },
-    {},
-    {num: 300},
-    {num: 30000},
-    {num: 30},
-    {searchValue: 'er'},
-    {persons: []},
-    {
-        cond: false,
-        persons: [
-            {name: 'errorrik', email: 'errorrik@gmail.com'},
-            {name: 'varsha', email: 'wangshuonpu@163.com'}
-        ]
-    },
-    {
-        persons: [
-            {name: 'otakustay', email: 'otakustay@gmail.com'},
-            {name: 'errorrik', email: 'errorrik@gmail.com'}
-        ]
-    },
-    {
-        num: 300
-    },
-    {
-        title: 'contributor',
-        name: 'errorrik',
-        closeText: 'X'
-    },
-    {
-        man: {
-            name: 'errorrik',
-            sex: 1,
-            email: 'errorrik@gmail.com'
-        }
-    },
-    {
-        man: {
-            name: 'errorrik',
-            sex: 1,
-            email: 'errorrik@gmail.com'
-        }
-    },
-    {
-        man: {
-            name: 'errorrik',
-            sex: 1,
-            email: 'errorrik@gmail.com'
-        }
-    },
-    {
-        desc: 'MVVM component framework',
-        name: 'San',
-        persons: [
-            {name: 'otakustay', email: 'otakustay@gmail.com'},
-            {name: 'errorrik', email: 'errorrik@gmail.com'}
-        ]
-    },
-    {
-        desc: 'MVVM component framework',
-        name: 'San',
-        num: 300
-    },
-    {
-        desc: 'MVVM component framework',
-        name: 'San'
-    },
-    {
-        desc: 'MVVM component framework',
-        name: 'San'
-    },
-    {
-        man: {
-            name: 'errorrik',
-            sex: 1,
-            email: 'errorrik@gmail.com'
-        }
-    },
-    {
-        cates: [
-            'foo',
-            'bar'
-        ],
-        forms: {
-            foo: [1, 2, 3],
-            bar: [4, 5, 6]
-        }
-    },
-    {
-        cates: [
-            'foo',
-            'bar'
-        ]
-    },
-    {
-        deps: [
-            {
-                strong: 'name',
-                columns: [
-                    {name: 'name', label: '名'},
-                    {name: 'email', label: '邮'}
-                ],
-                members: [
-                    {name: 'Justineo', email: 'justineo@gmail.com'},
-                    {name: 'errorrik', email: 'errorrik@gmail.com'}
-                ]
-            },
-            {
-                strong: 'email',
-                columns: [
-                    {name: 'name', label: '名'},
-                    {name: 'email', label: '邮'}
-                ],
-                members: [
-                    {name: 'otakustay', email: 'otakustay@gmail.com'},
-                    {name: 'leeight', email: 'leeight@gmail.com'}
-                ]
-            }
-        ]
-    },
-    {
-        man: {
-            name: 'errorrik',
-            sex: 1,
-            email: 'errorrik@gmail.com'
-        },
-        tip: 'tip'
-    },
-    {
-        man: {
-            name: 'errorrik',
-            sex: 1,
-            email: 'errorrik@gmail.com'
-        },
-        desc: 'tip'
-    },
-    {}
-];
-
-
-
-
 let htmlTpl = fs.readFileSync(path.resolve(__dirname, '../index-ssr.html.tpl'), 'UTF-8');
-let specTpl = fs.readFileSync(path.resolve(__dirname, 'ssr.spec.js.tpl'), 'UTF-8');
-
 let html = '';
+let specTpls = '';
 
-for (let i = 0; i < componentDatas.length; i++) {
-    let index = i + 1;
-    let ComponentClass = require('./cmpt' + index);
-    let renderer = san.compileToRenderer(ComponentClass);
-    // console.log('===================================')
-    // console.log(san.compileToSource(ComponentClass))
-    html += '<div id="wrap-cmpt' + index + '">'
-        + renderer(componentDatas[i])
+// generate html
+let genHtml = function ({componentClass, componentSource, compontentData, specTpl, dirName, result}) {
+    let renderer = san.compileToRenderer(componentClass);
+    let id = dirName;
+
+    // in no inject mark, add it
+    if (!/\/\/\s*\[inject\]/.test(specTpl)) {
+        specTpl = specTpl.replace(/function\s*\([a-z0-9_,$\s]*\)\s*\{/, function ($0) {
+            return $0 + '\n// [inject] init';
+        });
+    }
+
+    let injectHtml = result ? result : renderer(compontentData);
+
+    html += '<div id="' + id + '">'
+        + injectHtml
         + '</div>\n\n';
 
-    let componentFile = path.resolve(__dirname, 'cmpt' + index + '.js');
-    let componentSource = fs.readFileSync(componentFile, 'UTF-8')
-        .split('\n')
-        .map(line => {
-            if (/(\.|\s)exports\s*=/.test(line)
-                || /san\s*=\s*require\(/.test(line)
-            ) {
-                return ''
-            }
-
-            return line;
-        })
-        .join('\n');
     let preCode = `
         ${componentSource}
-        var wrap = document.getElementById('wrap-cmpt${index}');
+        var wrap = document.getElementById('${id}');
         var myComponent = new MyComponent({
             el: wrap.firstChild
         });
     `;
-    specTpl = specTpl.replace('##cmpt' + index + '##', preCode);
+    specTpl = specTpl.replace(/\/\/\s*\[inject\]\s* init/, preCode);
+    specTpls += specTpl;
+};
 
-}
+let buildFile = function (filePath) {
+    let files = fs.readdirSync(filePath);
+    let componentClass;
+    let componentSource;
+    let specTpl;
+    let compontentData;
+    let dirName;
+    let result;
+    let sourceFile = '';
 
+    if (!files.length) {
+        return;
+    }
 
-let karmaHtml = fs.readFileSync(path.resolve(__dirname, '../karma-context.html.tpl'), 'UTF-8');
-fs.writeFileSync(
-    path.resolve(__dirname, '../karma-context.html'),
-    karmaHtml.replace('##ssr-elements##', html),
-    'UTF-8'
-);
+    files.forEach(filename => {
+        // absolute path
+        let abFilePath = path.join(filePath, filename);
+        let stats = fs.statSync(abFilePath);
+        let isFile = stats.isFile();
+        let isDir = stats.isDirectory();
 
-fs.writeFileSync(
-    path.resolve(__dirname, '../index-ssr.html'),
-    htmlTpl.replace('##ssr-elements##', html),
-    'UTF-8'
-);
+        // if it's a file, init some data
+        if (isFile) {
+            // component file
+            if (filename === 'component.js') {
+                componentClass = require(abFilePath);
+                componentSource = fs.readFileSync(path.resolve(abFilePath), 'UTF-8')
+                    .split('\n')
+                    .map(line => {
+                        if (/(\.|\s)exports\s*=/.test(line)
+                            || /san\s*=\s*require\(/.test(line)
+                        ) {
+                            return '';
+                        }
 
-fs.writeFileSync(
-    path.resolve(__dirname, 'ssr.spec.js'),
-    specTpl,
-    'UTF-8'
-);
+                        return line;
+                    })
+                    .join('\n');
+                sourceFile = filename;
+            }
+            if (filename === 'spec.js') {
+                specTpl = fs.readFileSync(path.resolve(abFilePath), 'UTF-8');
+            }
+            if (filename === 'data.js') {
+                compontentData = require(abFilePath);
+            }
+            if (filename === 'result.html') {
+                result = fs.readFileSync(path.resolve(abFilePath), 'UTF-8').replace('\n', '');
+            }
+        }
+
+        // iterate
+        if (isDir) {
+            buildFile(abFilePath);
+        }
+    });
+
+    let match = filePath.match(/\/([a-z0-9_,$\-]*)$/);
+    // dirName is the identity of each component
+    dirName = match[1];
+    // generate html when it has source file
+    if (sourceFile) {
+        genHtml({
+            componentClass,
+            componentSource,
+            compontentData,
+            specTpl,
+            dirName,
+            result
+        });
+    }
+};
+
+let writeHtml = function ({htmlTpl, html, specTpls}) {
+    let karmaHtml = fs.readFileSync(path.resolve(__dirname, '../karma-context.html.tpl'), 'UTF-8');
+    fs.writeFileSync(
+        path.resolve(__dirname, '../karma-context.html'),
+        karmaHtml.replace('##ssr-elements##', html),
+        'UTF-8'
+    );
+
+    fs.writeFileSync(
+        path.resolve(__dirname, '../index-ssr.html'),
+        htmlTpl.replace('##ssr-elements##', html),
+        'UTF-8'
+    );
+
+    fs.writeFileSync(
+        path.resolve(__dirname, 'ssr.spec.js'),
+        specTpls,
+        'UTF-8'
+    );
+};
+
+buildFile(path.resolve(__dirname, './'));
+// write into html file
+writeHtml({htmlTpl, html, specTpls});
