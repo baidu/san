50dd7766fd2d4738c2544d7e09485f1daa2c5998
errorrik <errorrik@gmail.com>
2018-02-24 13:56:36 +0800

    remove IndexedList

--- src/main.js
+++ src/main.js
@@ -26,7 +26,6 @@
     // require('./browser/ie');
     // require('./browser/ie-old-than-9');
     // require('./browser/input-event-compatible');
-    // require('./util/indexed-list');
     // require('./browser/auto-close-tags');
     // require('./util/data-types.js');
     // require('./util/create-data-types-checker.js');
