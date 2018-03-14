23a18bb55ba22e71ad3fb5af7ef733823d0ddf21
errorrik <errorrik@gmail.com>
2018-03-14 11:45:09 +0800

    remove element-create

--- src/view/element-own-create.js
+++ src/view/element-own-create.js
@@ -3,14 +3,34 @@
  * @author errorrik(errorrik@gmail.com)
  */
 
-var elementCreate = require('./element-create');
 
 /**
  * 创建节点对应的 HTMLElement 主元素
  */
 function elementOwnCreate() {
     if (!this.lifeCycle.created) {
-        elementCreate(this);
+        this.lifeCycle = LifeCycle.painting;
+        this.el = createEl(this.tagName);
+        var isComponent = this.nodeType === NodeType.CMPT;
+
+        var hasIdDeclaration;
+
+        var me = this;
+        each(this.aNode.props, function (prop) {
+            var value = isComponent
+                ? evalExpr(prop.expr, me.data, me)
+                : evalExpr(prop.expr, me.scope, me.owner);
+
+            handleProp.prop(me, prop.name, value);
+
+            if (prop.name === 'id') {
+                me._elId = value;
+                hasIdDeclaration = true;
+            }
+        });
+
+        hasIdDeclaration || (this.el.id = this.id);
+
         this._toPhase('created');
     }
 }
