92ccb9dce3e3cefb25256b0553780ca360dd9002
errorrik <errorrik@gmail.com>
2018-04-24 23:36:02 +0800

    fix #276

--- src/util/decode-html-entity.js
+++ src/util/decode-html-entity.js
@@ -3,6 +3,20 @@
  * @author errorrik(errorrik@gmail.com)
  */
 
+var ENTITY_DECODE_MAP = {
+    lt: '<',
+    gt: '>',
+    nbsp: ' ',
+    quot: '\"',
+    emsp: '\u2003',
+    ensp: '\u2002',
+    thinsp: '\u2009',
+    copy: '\xa9',
+    reg: '\xae',
+    zwnj: '\u200c',
+    zwj: '\u200d',
+    amp: '&'
+};
 
 /**
  * 解码 HTML 字符实体
@@ -12,17 +26,15 @@
  */
 function decodeHTMLEntity(source) {
     return source
-        .replace(/&lt;/g, '<')
-        .replace(/&gt;/g, '>')
-        .replace(/&nbsp;/g, ' ')
-        .replace(/&quot;/g, '\"')
         .replace(/&#([0-9]+);/g, function (match, code) {
             return String.fromCharCode(+code);
         })
         .replace(/&#x([0-9a-f]+);/ig, function (match, code) {
             return String.fromCharCode(parseInt(code, 16));
         })
-        .replace(/&amp;/g, '&');
+        .replace(/&([a-z]+);/ig, function (match, code) {
+            return ENTITY_DECODE_MAP[code];
+        });
 }
 
 exports = module.exports = decodeHTMLEntity;
