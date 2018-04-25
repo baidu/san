3e85677bc4ac0dd5014521841e9cdca5379b035e
errorrik <errorrik@gmail.com>
2018-04-25 10:43:17 +0800

    opti test spec

--- test/element.spec.js
+++ test/element.spec.js
@@ -759,7 +759,7 @@ describe("Element", function () {
     });
 
     it("html entity support is limited", function () {
-        var entityStr = '&#39;&#x00021;&emsp;&ensp;&thinsp;&copy;&reg;&zwnj;&zwj;&lt;&nbsp;&gt;&quot;';
+        var entityStr = '&#39;&#x00021;&emsp;&ensp;&thinsp;&copy;&lt;p&gt;&reg;&lt;/p&gt;&reg;&zwnj;&zwj;&lt;&nbsp;&gt;&quot;';
         var MyComponent = san.defineComponent({
             template: '<u>' + entityStr + '</u>'
         });
