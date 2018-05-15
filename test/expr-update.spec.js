98596a72c02c01f09d1932c16d5f59667e35510d
errorrik <errorrik@gmail.com>
2018-05-15 12:32:42 +0800

    array spread with push

--- test/expr-update.spec.js
+++ test/expr-update.spec.js
@@ -743,10 +743,25 @@ describe("Expression Update Detect", function () {
             expect(lis[5].innerHTML).toBe('5');
             expect(lis[6].innerHTML).toBe('6');
 
-            myComponent.dispose();
-            document.body.removeChild(wrap);
+            myComponent.data.push('ext', 10);
 
-            done();
+            san.nextTick(function () {
+                expect(lis[0].innerHTML).toBe('1');
+                expect(lis[1].innerHTML).toBe('true');
+                expect(lis[2].innerHTML).toBe('3');
+                expect(lis[3].innerHTML).toBe('4');
+
+                expect(lis[4].innerHTML).toBe('10');
+                expect(lis[5].innerHTML).toBe('erik');
+                expect(lis[6].innerHTML).toBe('5');
+                expect(lis[7].innerHTML).toBe('6');
+
+                myComponent.dispose();
+                document.body.removeChild(wrap);
+
+                done();
+            })
+
         });
     });
 
