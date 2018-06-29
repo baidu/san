1ebd7c608f617f1007f00fbf7acfb96eb8887c1e
errorrik <errorrik@gmail.com>
2018-06-29 14:54:49 +0800

    add render object spec

--- test/for-directive.spec.js
+++ test/for-directive.spec.js
@@ -2285,4 +2285,58 @@ describe("ForDirective", function () {
             done();
         });
     });
+
+    it("render object", function (done) {
+        var MyComponent = san.defineComponent({
+            template: '<ul><li san-for="p,i in persons" title="{{p}}">{{p}}-{{i}}</li></ul>'
+        });
+
+        var myComponent = new MyComponent({
+            data: {
+                persons: {
+                    'erik': 'errorrik@gmail.com',
+                    'otakustay': 'otakustay@gmail.com'
+                }
+            }
+        });
+
+        var erikHTML = 'erik-errorrik@gmail.com';
+        var grayHTML = 'otakustay-otakustay@gmail.com';
+        var leeHTML = 'leeight-leeight@gmail.com';
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var lis = wrap.getElementsByTagName('li');
+
+        expect(lis.length).toBe(2);
+        expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML).toBeTrurhy();
+        expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML).toBeTrurhy();
+
+        myComponent.data.set('persons.leeight', 'leeight@gmail.com');
+        myComponent.nextTick(function () {
+            var lis = wrap.getElementsByTagName('li');
+
+            expect(lis.length).toBe(3);
+            expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTrurhy();
+            expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTrurhy();
+            expect(lis[2].innerHTML === erikHTML || lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTrurhy();
+
+            myComponent.data.set('persons.erik', null);
+            myComponent.nextTick(function () {
+                var lis = wrap.getElementsByTagName('li');
+
+                expect(lis.length).toBe(2);
+                expect(lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTrurhy();
+                expect(lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTrurhy();
+                expect(lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTrurhy();
+
+                myComponent.dispose();
+                document.body.removeChild(wrap);
+                done();
+            });
+        });
+
+    });
 });
