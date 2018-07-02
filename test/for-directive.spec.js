04815796df7ae7e001599ab2df6bf0ef1d76c0f2
errorrik <errorrik@gmail.com>
2018-07-02 11:07:43 +0800

    literal

--- test/for-directive.spec.js
+++ test/for-directive.spec.js
@@ -2288,7 +2288,7 @@ describe("ForDirective", function () {
 
     it("render object", function (done) {
         var MyComponent = san.defineComponent({
-            template: '<ul><li san-for="p,i in persons" title="{{p}}">{{p}}-{{i}}</li></ul>'
+            template: '<ul><li san-for="p,i in persons" title="{{p}}">{{i}}-{{p}}</li></ul>'
         });
 
         var myComponent = new MyComponent({
@@ -2311,26 +2311,25 @@ describe("ForDirective", function () {
         var lis = wrap.getElementsByTagName('li');
 
         expect(lis.length).toBe(2);
-        expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML).toBeTrurhy();
-        expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML).toBeTrurhy();
+        expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML).toBeTruthy();
+        expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML).toBeTruthy();
 
         myComponent.data.set('persons.leeight', 'leeight@gmail.com');
         myComponent.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
 
             expect(lis.length).toBe(3);
-            expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTrurhy();
-            expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTrurhy();
-            expect(lis[2].innerHTML === erikHTML || lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTrurhy();
+            expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTruthy();
+            expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTruthy();
+            expect(lis[2].innerHTML === erikHTML || lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTruthy();
 
             myComponent.data.set('persons.erik', null);
             myComponent.nextTick(function () {
                 var lis = wrap.getElementsByTagName('li');
 
                 expect(lis.length).toBe(2);
-                expect(lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTrurhy();
-                expect(lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTrurhy();
-                expect(lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTrurhy();
+                expect(lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTruthy();
+                expect(lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTruthy();
 
                 myComponent.dispose();
                 document.body.removeChild(wrap);
