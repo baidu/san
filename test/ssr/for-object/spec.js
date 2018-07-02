9f1858bce5fd5524c0d197cfeaaaaa5d820d0d82
errorrik <errorrik@gmail.com>
2018-07-02 11:07:57 +0800

    fix spec bug

--- test/ssr/for-object/spec.js
+++ test/ssr/for-object/spec.js
@@ -9,26 +9,25 @@ it("for render object", function (done) {
     var lis = wrap.getElementsByTagName('li');
 
     expect(lis.length).toBe(2);
-    expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML).toBeTrurhy();
-    expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML).toBeTrurhy();
+    expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML).toBeTruthy();
+    expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML).toBeTruthy();
 
     myComponent.data.set('persons.leeight', 'leeight@gmail.com');
     myComponent.nextTick(function () {
         var lis = wrap.getElementsByTagName('li');
 
         expect(lis.length).toBe(3);
-        expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTrurhy();
-        expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTrurhy();
-        expect(lis[2].innerHTML === erikHTML || lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTrurhy();
+        expect(lis[0].innerHTML === erikHTML || lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTruthy();
+        expect(lis[1].innerHTML === erikHTML || lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTruthy();
+        expect(lis[2].innerHTML === erikHTML || lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTruthy();
 
         myComponent.data.set('persons.erik', null);
         myComponent.nextTick(function () {
             var lis = wrap.getElementsByTagName('li');
 
             expect(lis.length).toBe(2);
-            expect(lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTrurhy();
-            expect(lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTrurhy();
-            expect(lis[2].innerHTML === grayHTML || lis[2].innerHTML === leeHTML).toBeTrurhy();
+            expect(lis[0].innerHTML === grayHTML || lis[0].innerHTML === leeHTML).toBeTruthy();
+            expect(lis[1].innerHTML === grayHTML || lis[1].innerHTML === leeHTML).toBeTruthy();
 
             myComponent.dispose();
             document.body.removeChild(wrap);
