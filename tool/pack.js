e7356e6f9ad78a97241ae689d4100c6be8b20dc4
errorrik <errorrik@gmail.com>
2018-09-19 17:58:36 +0800

    opti pack script

--- tool/pack.js
+++ tool/pack.js
@@ -4,10 +4,27 @@ const path = require('path');
 
 // 匹配 require
 const REQ_RULE = /(=|^|\s)require\(\s*(['"])([^'"]+)['"]\s*\)/;
-// 匹配枚举类型依赖，规则为以 `-type.js` 结尾的文件
-const ENUM_TYPE_RULE = /\/([^\/]+-type)\.js/;
-// 记录枚举类型变量的 map
-let enumTypes = {};
+
+// 需要优化的 enum 变量
+const OPTI_ENUM = {
+    ExprType: require('../src/parser/expr-type'),
+    NodeType: require('../src/view/node-type'),
+    DataChangeType: require('../src/runtime/data-change-type')
+};
+
+const OPTI_ENUM_REPLACE_REG = new RegExp(
+    '(' + Object.keys(OPTI_ENUM).join('|') + ')\\.([A-Z_]+)',
+    'g'
+);
+
+function optiEnumReplaceFn(match, type, prop) {
+    if (!(prop in OPTI_ENUM[type])) {
+        throw new Error(`[ENUM OPTI ERROR] ${type} has not property ${prop}`);
+    }
+
+    return OPTI_ENUM[type][prop];
+}
+
 
 function pack(rootDir, mainFile) {
     let srcDir = path.resolve(rootDir, 'src');
@@ -18,7 +35,7 @@ function pack(rootDir, mainFile) {
 
     return fileContent(mainFile, 1, []).replace(
         '// #[main-dependencies]',
-        deps.map(dep => fileContent(dep.file, 0, dep.enum)).join('\n\n')
+        deps.map(dep => fileContent(dep)).join('\n\n')
     );
 }
 
@@ -30,18 +47,12 @@ function depAnalyse(targetFile) {
         if (depsIndex[file]) {
             if (depsIndex[file] === 1) {
                 if (file !== targetFile) {
-                    deps.push({
-                        file: file,
-                        enum: Object.keys(enumTypes)
-                    });
+                    deps.push(file);
                 }
                 depsIndex[file] = 2;
             }
             return;
         }
-
-        // 用于记录当前文件，依赖了哪些枚举类型
-        let localEnumDeps = [];
 
         depsIndex[file] = 1;
 
@@ -51,30 +62,12 @@ function depAnalyse(targetFile) {
 
             if (requireMatch) {
                 let dep = resolveDep(requireMatch[3], file);
-                let enumTypeMatch = ENUM_TYPE_RULE.exec(dep);
-                // 有枚举类型依赖
-                if (enumTypeMatch) {
-                    // 获取枚举变量名
-                    let namespace = kebab2upperCamel(enumTypeMatch[1]);
-                    // 获取枚举值
-                    let namespaceEnum = require(dep);
-                    // 记录相关信息
-                    if (!enumTypes[namespace]) {
-                        enumTypes[namespace] = namespaceEnum;
-                    }
-                    localEnumDeps.push(namespace);
-                }
                 analyse(dep);
             }
         });
 
         if (file !== targetFile && depsIndex[file] !== 2) {
-            deps.push({
-                // 依赖文件路径
-                file: file,
-                // 涉及到的枚举类型依赖
-                enum: localEnumDeps
-            });
+            deps.push(file);
         }
         depsIndex[file] = 2;
     }
@@ -99,25 +92,10 @@ function resolveDep(dep, inFile) {
  *
  * @param {string} file 文件路径
  * @param {boolean} dontIgnoreExports 产出是否不忽略 exports 语句
- * @param {string[]} localEnumDeps 文件所依赖的枚举变量列表
  * @return {string} 处理后的源码
  */
-function fileContent(file, dontIgnoreExports, localEnumDeps) {
+function fileContent(file, dontIgnoreExports) {
     return fs.readFileSync(file, 'UTF-8').split(/\r?\n/)
-        .map(line => {
-            if (localEnumDeps.length === 0) {
-                return line;
-            }
-            for (let i = 0; i < localEnumDeps.length; i++) {
-                let namespace = localEnumDeps[i];
-                for (let enumName in enumTypes[namespace]) {
-                    let replacer = namespace + '.' + enumName;
-                    let replaceValue = enumTypes[namespace][enumName];
-                    line = line.replace(replacer, replaceValue);
-                }
-            }
-            return line;
-        })
         .map(line => {
             if (/(=|^|\s)require\(['"]/.test(line)
                 || (!dontIgnoreExports && /^\s*(module\.)?exports\s+=/.test(line))
@@ -125,21 +103,10 @@ function fileContent(file, dontIgnoreExports, localEnumDeps) {
                 return '// ' + line;
             }
 
-            return line;
+            return line.replace(OPTI_ENUM_REPLACE_REG, optiEnumReplaceFn);
         })
         .join('\n');
 }
 
-/**
- * kebab 命名转大坨峰命名
- *
- * @param {string} source 源字符串
- * @return {string} 大坨峰格式字符串
- */
-function kebab2upperCamel(source) {
-    return source.replace(/(-|^)([a-z])/g, function (match, alpha, beta) {
-        return beta.toUpperCase();
-    });
-}
 
 exports = module.exports = pack;
