3a598494082e16ae1b656a84667366ac59c2efcd
errorrik <errorrik@gmail.com>
2018-10-23 12:19:06 +0800

    async component base spec

--- test/component.spec.js
+++ test/component.spec.js
@@ -3739,5 +3739,56 @@ describe("Component", function () {
         });
 
     });
+
+    it("async component", function (done) {
+        var Label = san.defineComponent({
+            template: '<u>{{text}}</u>'
+        });
+
+        var loadSuccess;
+        var MyComponent = san.defineComponent({
+            components: {
+                'x-label': {
+                    load: function () {
+                        return {
+                            then: function (success) {
+                                loadSuccess = success;
+                            }
+                        };
+                    }
+                }
+            },
+
+            template: '<div><x-label text="{{text}}"/></div>',
+
+            attached: function () {
+                this.nextTick(function () {
+                    loadSuccess(Label);
+                });
+            }
+        });
+
+        var myComponent = new MyComponent({
+            data: {
+                text: 'Hello San'
+            }
+        });
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        expect(wrap.getElementsByTagName('u').length).toBe(0);
+
+        setTimeout(function () {
+            expect(wrap.getElementsByTagName('u').length).toBe(1);
+            expect(wrap.getElementsByTagName('u')[0].innerHTML).toBe('Hello San');
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        }, 500);
+
+    });
 });
 
