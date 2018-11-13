97ccd3bc0deabeb6c3b2c757660e902da4ff5ca8
errorrik <errorrik@gmail.com>
2018-11-13 21:51:05 +0800

    skeleton of component loader

--- src/view/component-loader.js
+++ src/view/component-loader.js
@@ -2,9 +2,16 @@
 
 function ComponentLoader(options) {
     this.options = options;
+    this.id = guid();
 }
 
+ComponentLoader.prototype._create = nodeOwnCreateStump;
 
+ComponentLoader.prototype.attach = function (parentEl, beforeEl) {
+    this._create();
+    insertBefore(this.el, parentEl, beforeEl);
+};
 
+ComponentLoader.prototype.dispose = nodeOwnSimpleDispose;
 
 exports = module.exports = ComponentLoader;
