6b64d94c3bcbbcb7e22923fef14cb3a21c3a37a2
errorrik <errorrik@gmail.com>
2018-11-14 15:32:53 +0800

    add loading spec

--- test/component-async.spec.js
+++ test/component-async.spec.js
@@ -50,4 +50,65 @@ describe("Component Async", function () {
         }, 500);
 
     });
+
+
+    it("loading placeholder", function (done) {
+        var Label = san.defineComponent({
+            template: '<u>{{text}}</u>'
+        });
+
+        var LoadingLabel = san.defineComponent({
+            template: '<b>{{text}}</b>'
+        });
+
+        var loadSuccess;
+        var MyComponent = san.defineComponent({
+            components: {
+                'x-label': {
+                    load: function () {
+                        return {
+                            then: function (success) {
+                                loadSuccess = success;
+                            }
+                        };
+                    },
+                    loading: LoadingLabel
+                }
+            },
+
+            template: '<div><x-label text="{{text}}"/></div>',
+
+            attached: function () {
+                this.nextTick(function () {
+                    loadSuccess(Label);
+                });
+            }
+        });
+
+        var myComponent = new MyComponent({
+            data: {
+                text: 'Hello San'
+            }
+        });
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        expect(wrap.getElementsByTagName('u').length).toBe(0);
+        expect(wrap.getElementsByTagName('b').length).toBe(1);
+        expect(wrap.getElementsByTagName('b')[0].innerHTML).toBe('Hello San');
+
+        setTimeout(function () {
+            expect(wrap.getElementsByTagName('u').length).toBe(1);
+            expect(wrap.getElementsByTagName('u')[0].innerHTML).toBe('Hello San');
+
+            expect(wrap.getElementsByTagName('b').length).toBe(0);
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        }, 500);
+
+    });
 });
