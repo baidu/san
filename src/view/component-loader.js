1d71a8a2108d6c3aa28b2a2ca80f139223bef824
errorrik <errorrik@gmail.com>
2018-11-14 11:35:19 +0800

    rough implement 4 #339

--- src/view/component-loader.js
+++ src/view/component-loader.js
@@ -10,8 +10,42 @@ ComponentLoader.prototype._create = nodeOwnCreateStump;
 ComponentLoader.prototype.attach = function (parentEl, beforeEl) {
     this._create();
     insertBefore(this.el, parentEl, beforeEl);
+
+    var startLoad = this.load();
+    if (typeof startLoad.then === 'function') {
+        var me = this;
+        startLoad.then(
+            function (RealComponent) {
+                me.done(RealComponent);
+            },
+            function (RealComponent) {
+                me.done(RealComponent)
+            }
+        );
+    }
 };
 
 ComponentLoader.prototype.dispose = nodeOwnSimpleDispose;
+
+ComponentLoader.prototype.done = function (ComponentClass) {
+    if (this.el) {
+        ComponentClass = ComponentClass || this.fallback;
+
+        var component = new ComponentClass(this.options);
+        component.attach(this.el.parentNode, this.el);
+
+        var parentChildren = this.options.parent.children;
+        var parentChildrenLen = parentChildren.length;
+
+        while (parentChildrenLen--) {
+            if (parentChildren[parentChildrenLen] === this) {
+                parentChildren[parentChildrenLen] = component;
+                break;
+            }
+        }
+    }
+
+    this.dispose();
+};
 
 exports = module.exports = ComponentLoader;
