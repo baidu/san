1c42208471f0daed70ebc10e79bdc1943fa8a822
errorrik <errorrik@gmail.com>
2018-11-18 19:07:51 +0800

    add async component specs

--- test/component-async.spec.js
+++ test/component-async.spec.js
@@ -260,4 +260,147 @@ describe("Component Async", function () {
         }, 500);
 
     });
+
+    it("with if", function (done) {
+        var Label = san.defineComponent({
+            template: '<u>{{text}}</u>'
+        });
+
+        var LoadingLabel = san.defineComponent({
+            template: '<b>{{text}}</b>'
+        });
+
+        var loadSuccess;
+        var MyComponent = san.defineComponent({
+            components: {
+                'x-label': {
+                    load: function () {
+                        return {
+                            then: function (success) {
+                                loadSuccess = success;
+                            }
+                        };
+                    },
+                    loading: LoadingLabel
+                }
+            },
+
+            template: '<div><x-label text="{{text}}" s-if="isShow"/></div>'
+        });
+
+        var myComponent = new MyComponent({
+            data: {
+                text: 'Hello San'
+            }
+        });
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+
+        expect(wrap.getElementsByTagName('u').length).toBe(0);
+        expect(wrap.getElementsByTagName('b').length).toBe(0);
+        myComponent.data.set('isShow', true);
+        myComponent.nextTick(function () {
+            expect(wrap.getElementsByTagName('u').length).toBe(0);
+            expect(wrap.getElementsByTagName('b').length).toBe(1);
+            expect(wrap.getElementsByTagName('b')[0].innerHTML).toBe('Hello San');
+
+            loadSuccess(Label);
+            myComponent.nextTick(function () {
+                expect(wrap.getElementsByTagName('u').length).toBe(1);
+                expect(wrap.getElementsByTagName('u')[0].innerHTML).toBe('Hello San');
+                expect(wrap.getElementsByTagName('b').length).toBe(0);
+
+                myComponent.data.set('isShow', false);
+                myComponent.nextTick(function () {
+                    expect(wrap.getElementsByTagName('u').length).toBe(0);
+                    expect(wrap.getElementsByTagName('b').length).toBe(0);
+                    myComponent.data.set('isShow', true);
+
+                    myComponent.nextTick(function () {
+                        expect(wrap.getElementsByTagName('u').length).toBe(0);
+                        expect(wrap.getElementsByTagName('b').length).toBe(1);
+                        expect(wrap.getElementsByTagName('b')[0].innerHTML).toBe('Hello San');
+
+                        myComponent.nextTick(function () {
+                            expect(wrap.getElementsByTagName('u').length).toBe(1);
+                            expect(wrap.getElementsByTagName('u')[0].innerHTML).toBe('Hello San');
+                            expect(wrap.getElementsByTagName('b').length).toBe(0);
+
+                            myComponent.dispose();
+                            document.body.removeChild(wrap);
+                            done();
+                        });
+
+
+                    });
+                });
+
+            });
+        })
+
+
+    });
+
+    it("with slot", function (done) {
+        var Label = san.defineComponent({
+            template: '<u><slot/></u>'
+        });
+
+        var LoadingLabel = san.defineComponent({
+            template: '<b><slot/></b>'
+        });
+
+        var loadSuccess;
+        var MyComponent = san.defineComponent({
+            components: {
+                'x-label': {
+                    load: function () {
+                        return {
+                            then: function (success) {
+                                loadSuccess = success;
+                            }
+                        };
+                    },
+                    loading: LoadingLabel
+                }
+            },
+
+            template: '<div><x-label>Hello {{text}}</x-label></div>',
+
+            attached: function () {
+                this.nextTick(function () {
+                    loadSuccess(Label);
+                });
+            }
+        });
+
+        var myComponent = new MyComponent({
+            data: {
+                text: 'San'
+            }
+        });
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        expect(wrap.getElementsByTagName('u').length).toBe(0);
+        expect(wrap.getElementsByTagName('b').length).toBe(1);
+        expect(wrap.getElementsByTagName('b')[0].innerHTML).toContain('Hello San');
+
+        setTimeout(function () {
+            expect(wrap.getElementsByTagName('u').length).toBe(1);
+            expect(wrap.getElementsByTagName('u')[0].innerHTML).toContain('Hello San');
+
+            expect(wrap.getElementsByTagName('b').length).toBe(0);
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        }, 500);
+
+    });
 });
