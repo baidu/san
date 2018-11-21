e23b82044b472921f25bd2509c51aaa04af5dfc5
errorrik <errorrik@gmail.com>
2018-11-21 09:56:49 +0800

    options is function, tobe options.load

--- src/view/create-component-loader.js
+++ src/view/create-component-loader.js
@@ -8,16 +8,18 @@ var ComponentLoader = require('./component-loader');
 /**
  * 创建组件Loader
  *
- * @param {Object} options loader参数
+ * @param {Object|Function} options 创建组件Loader的参数。为Object时参考下方描述，为Function时代表load方法。
  * @param {Function} options.load load方法
- * @param {Function=} options.loading loading过程中渲染的组件
+ * @param {Function=} options.placeholder loading过程中渲染的占位组件
  * @param {Function=} options.fallback load失败时渲染的组件
  * @return {ComponentLoader}
  */
 function createComponentLoader(options) {
-    if (options && typeof options.load === 'function') {
-        return new ComponentLoader(options);
-    }
+    var placeholder = options.placeholder;
+    var fallback = options.fallback;
+    var load = typeof options === 'function' ? options : options.load;
+
+    return new ComponentLoader(load, placeholder, fallback);
 }
 
 exports = module.exports = createComponentLoader;
