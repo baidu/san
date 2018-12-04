3c3f1fccf8fd8ceb1184da97f85640646794991c
xtx1130 <xtx1130@gmail.com>
2018-12-04 21:15:39 +0800

    lint: fix lint

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -86,7 +86,7 @@ module.exports = function(config) {
         reports:{
             html: 'coverage/html',
             lcovonly: 'coverage/report-lcov/lcov.info',
-      }
+        }
     };
 
   }
