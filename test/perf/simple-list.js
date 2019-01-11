ae32a821fc5279c776cb091f05fa0b4eb2807dc7
errorrik <errorrik@gmail.com>
2019-01-11 10:56:58 +0800

    multi component

--- test/perf/simple-list.js
+++ test/perf/simple-list.js
@@ -7,6 +7,16 @@ let App = san.defineComponent({
     template: `<div id='app'><ul><li s-for='item in items'>{{item}}</li></ul></div>`
 });
 
+let Item = san.defineComponent({
+    template: `<li>{{item}}</li>`
+});
+let App2 = san.defineComponent({
+    components: {
+        'x-item': Item
+    },
+    template: `<div id='app'><ul><x-item s-for='item in items' item="{{item}}"/></ul></div>`
+});
+
 
 var items = [];
 for (var i = 0; i < 10000; i++) {
@@ -17,6 +27,7 @@ var data = {items};
 
 
 var renderer = san.compileToRenderer(App);
+var renderer2 = san.compileToRenderer(App2);
 var swigRenderer = swig.compile(`<div id='app'><ul>{% for item in items %}<li>{{item}}</li>{% endfor %}</ul></div>`);
 var artRenderer = art.compile(`<div id='app'><ul>{<% for(var i = 0; i < items.length; i++){ %><li><%= items[i] %></li><% } %></ul></div>`)
 
@@ -29,6 +40,13 @@ for (var i = 0; i < 100; i++) {
 }
 var runtime2 = (new Date) - now2;
 console.log(`san: ${runtime2 / 100}ms`);
+
+var now1 = new Date();
+for (var i = 0; i < 100; i++) {
+    renderer2(data);
+}
+var runtime1 = (new Date) - now1;
+console.log(`san(item as component): ${runtime1 / 100}ms`);
 
 var now = new Date();
 for (var i = 0; i < 100; i++) {
