90b6ed2724e6857b92dfdfaddea54bac08e9b189
errorrik <errorrik@gmail.com>
2019-01-14 15:36:21 +0800

    mv compileSourcePreCode

--- src/view/compile-source-buffer.js
+++ src/view/compile-source-buffer.js
@@ -50,6 +50,13 @@ CompileSourceBuffer.prototype.joinRaw = function (code) {
  */
 CompileSourceBuffer.prototype.addRendererStart = function () {
     this.addRaw('function (data) {');
+    this.addRaw(
+        compileSourcePreCode.toString()
+            .split('\n')
+            .slice(1)
+            .join('\n')
+            .replace(/\}\s*$/, '')
+    );
 };
 
 /**
@@ -142,6 +149,211 @@ CompileSourceBuffer.prototype.toCode = function () {
 
     return code.join('\n');
 };
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable fecs-camelcase */
+
+/**
+ * 组件编译的模板函数
+ *
+ * @inner
+ */
+function compileSourcePreCode() {
+    var $version = '##version##';
+
+    var componentRenderers = {};
+
+    function extend(target, source) {
+        if (source) {
+            Object.keys(source).forEach(function (key) {
+                let value = source[key];
+                if (typeof value !== 'undefined') {
+                    target[key] = value;
+                }
+            });
+        }
+
+        return target;
+    }
+
+    function each(array, iterator) {
+        if (array && array.length > 0) {
+            for (var i = 0, l = array.length; i < l; i++) {
+                if (iterator(array[i], i) === false) {
+                    break;
+                }
+            }
+        }
+    }
+
+    function contains(array, value) {
+        var result;
+        each(array, function (item) {
+            result = item === value;
+            return !result;
+        });
+
+        return result;
+    }
+
+    var HTML_ENTITY = {
+        /* jshint ignore:start */
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        /* eslint-disable quotes */
+        "'": '&#39;'
+        /* eslint-enable quotes */
+        /* jshint ignore:end */
+    };
+
+    function htmlFilterReplacer(c) {
+        return HTML_ENTITY[c];
+    }
+
+    function escapeHTML(source) {
+        if (source == null) {
+            return '';
+        }
+
+        return String(source).replace(/[&<>"']/g, htmlFilterReplacer);
+    }
+
+    var DEFAULT_FILTERS = {
+        url: encodeURIComponent,
+        _class: function (source) {
+            if (source instanceof Array) {
+                return source.join(' ');
+            }
+
+            return source;
+        },
+        _style: function (source) {
+            if (typeof source === 'object') {
+                var result = '';
+                if (source) {
+                    Object.keys(source).forEach(function (key) {
+                        result += key + ':' + source[key] + ';';
+                    });
+                }
+
+                return result;
+            }
+
+            return source || '';
+        },
+        _sep: function (source, sep) {
+            return source ? sep + source : '';
+        }
+    };
+
+    function attrFilter(name, value) {
+        if (value) {
+            return ' ' + name + '="' + value + '"';
+        }
+
+        return '';
+    }
+
+    function boolAttrFilter(name, value) {
+        if (value && value !== 'false' && value !== '0') {
+            return ' ' + name;
+        }
+
+        return '';
+    }
+
+    function stringLiteralize(source) {
+        return '"'
+            + source
+                .replace(/\x5C/g, '\\\\')
+                .replace(/"/g, '\\"')
+                .replace(/\x0A/g, '\\n')
+                .replace(/\x09/g, '\\t')
+                .replace(/\x0D/g, '\\r')
+            + '"';
+    }
+
+    var stringifier = {
+        obj: function (source) {
+            var prefixComma;
+            var result = '{';
+
+            Object.keys(source).forEach(function (key) {
+                if (typeof source[key] === 'undefined') {
+                    return;
+                }
+
+                if (prefixComma) {
+                    result += ',';
+                }
+                prefixComma = 1;
+
+                result += stringLiteralize(key) + ':' + stringifier.any(source[key]);
+            });
+
+            return result + '}';
+        },
+
+        arr: function (source) {
+            var prefixComma;
+            var result = '[';
+
+            each(source, function (value) {
+                if (prefixComma) {
+                    result += ',';
+                }
+                prefixComma = 1;
+
+                result += stringifier.any(value);
+            });
+
+            return result + ']';
+        },
+
+        str: function (source) {
+            return stringLiteralize(source);
+        },
+
+        date: function (source) {
+            return 'new Date(' + source.getTime() + ')';
+        },
+
+        any: function (source) {
+            switch (typeof source) {
+                case 'string':
+                    return stringifier.str(source);
+
+                case 'number':
+                    return '' + source;
+
+                case 'boolean':
+                    return source ? 'true' : 'false';
+
+                case 'object':
+                    if (!source) {
+                        return null;
+                    }
+
+                    if (source instanceof Array) {
+                        return stringifier.arr(source);
+                    }
+
+                    if (source instanceof Date) {
+                        return stringifier.date(source);
+                    }
+
+                    return stringifier.obj(source);
+            }
+
+            throw new Error('Cannot Stringify:' + source);
+        }
+    };
+}
+/* eslint-enable no-unused-vars */
+/* eslint-enable fecs-camelcase */
+
 
 // #[end]
 
