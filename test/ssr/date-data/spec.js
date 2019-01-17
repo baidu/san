1563a322e190625699ed2a3e0948edd3d1711a3e
errorrik <errorrik@gmail.com>
2019-01-17 09:03:59 +0800

    check date data item

--- test/ssr/date-data/spec.js
+++ test/ssr/date-data/spec.js
@@ -4,7 +4,8 @@ it("date data", function (done) {
     var b = wrap.getElementsByTagName('b')[0];
     expect(b.title).toBe('1983');
 
-    myComponent.data.set('date', new Date(1984, 10, 10));
+
+    myComponent.data.set('date', new Date(myComponent.data.get('date').getFullYear() + 1, 10, 10));
 
     san.nextTick(function () {
         var b = wrap.getElementsByTagName('b')[0];
