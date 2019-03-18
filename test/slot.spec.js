7906a05b100c9cb968eca463b602d76be5f710da
errorrik <errorrik@gmail.com>
2019-03-18 09:22:20 +0800

    add s-bind and var- confilct specs

--- test/slot.spec.js
+++ test/slot.spec.js
@@ -1974,6 +1974,53 @@ describe("Slot", function () {
         })
     });
 
+    it("scoped by given content with s-bind and var-, both dynamic data ref", function (done) {
+        var Man = san.defineComponent({
+            template: '<div><slot name="test" s-bind="{n:data.name, email: data.email, sex: \'shemale\'}" var-email="data.email2" var-sex="data.sex ? \'male\' : \'female\'"><p>{{n}},{{sex}},{{email}}</p></slot></div>'
+        });
+
+        var MyComponent = san.defineComponent({
+            components: {
+                'x-man': Man
+            },
+
+            template: '<div><x-man data="{{man}}"><h3 slot="test">{{n}}</h3><b slot="test">{{sex}}</b><u slot="test">{{email}}</u></x-man></div>',
+
+            initData: function () {
+                return {
+                    man: {
+                        name: 'errorrik',
+                        sex: 1,
+                        email: 'errorrik@gmail.com',
+                        email2: 'errorrik2@gmail.com'
+                    }
+                };
+            }
+        });
+
+        var myComponent = new MyComponent();
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        expect(wrap.getElementsByTagName('h3')[0].innerHTML).toBe('errorrik');
+        expect(wrap.getElementsByTagName('b')[0].innerHTML).toBe('male');
+        expect(wrap.getElementsByTagName('u')[0].innerHTML).toBe('errorrik2@gmail.com');
+        myComponent.data.set('man.email', 'erik168@163.com');
+        myComponent.data.set('man.email2', 'erik1682@163.com');
+        san.nextTick(function () {
+
+            expect(wrap.getElementsByTagName('h3')[0].innerHTML).toBe('errorrik');
+            expect(wrap.getElementsByTagName('b')[0].innerHTML).toBe('male');
+            expect(wrap.getElementsByTagName('u')[0].innerHTML).toBe('erik1682@163.com');
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        })
+    });
+
     it("deep", function (done) {
         var Panel = san.defineComponent({
             template: '<div><slot/></div>'
