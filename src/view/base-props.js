ee5baa0540034f8ffc3343cc497e787eedfe1920
errorrik <errorrik@gmail.com>
2019-05-07 13:26:33 +0800

    add base props

---
+++ src/view/base-props.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Baidu Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license.
+ * See LICENSE file in the project root for license information.
+ *
+ * @file 元素的基本属性
+ */
+
+var baseProps = {
+    'class': 1,
+    'style': 1,
+    'id': 1
+};
+
+exports = module.exports = baseProps;
