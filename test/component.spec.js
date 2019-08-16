afe8621fd6742b0715ba66f433f9583ca194ab2b
errorrik <errorrik@gmail.com>
2019-08-16 15:47:28 +0800

    add #433 spec

--- test/component.spec.js
+++ test/component.spec.js
@@ -4807,5 +4807,45 @@ describe("Component", function () {
         myComponent.dispose();
         document.body.removeChild(wrap);
     });
+
+    it("outer class merge inner class", function (done) {
+        var Child = san.defineComponent({
+            template: '<b class="{{clazz}}">{{text}}</b>'
+        });
+
+        var MyComponent = san.defineComponent({
+            template: '<div><x-child class="{{clazz}}" s-ref="child" /></div>',
+            components: {
+                'x-child': Child
+            }
+        });
+
+        var myComponent = new MyComponent({
+            data: {
+                clazz: 'outer'
+            }
+        });
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        expect(myComponent.ref('child').el.className).toBe('outer');
+        myComponent.ref('child').data.set('clazz', ['inn1', 'inn2']);
+
+        myComponent.nextTick(function () {
+            expect(myComponent.ref('child').el.className).toBe('inn1 inn2 outer');
+            myComponent.data.set('clazz', '');
+
+            myComponent.nextTick(function () {
+                expect(myComponent.ref('child').el.className).toBe('inn1 inn2');
+
+
+                myComponent.dispose();
+                document.body.removeChild(wrap);
+                done();
+            });
+        });
+    });
 });
 
