68efe0295520413883c4ba35eedee24246e97134
errorrik <errorrik@gmail.com>
2019-11-18 12:02:43 +0800

    add #442 spec

--- test/element.spec.js
+++ test/element.spec.js
@@ -945,4 +945,52 @@ describe("Element", function () {
         myComponent.dispose();
         document.body.removeChild(wrap);
     });
+
+    it("content attr with only one expr interp", function (done) {
+        var MyComponent = san.defineComponent({
+            template: '<div undef="{{undef}}" nul="{{nul}}" falsy="{{falsy}}" truth="{{truth}}" estr="{{estr}}" zero="{{0}}">test</div>'
+        });
+        var myComponent = new MyComponent({
+            data: {
+                nul: null,
+                falsy: false,
+                truth: true,
+                estr: ''
+            }
+        });
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        expect(myComponent.el.hasAttribute('undef')).toBeFalsy();
+        expect(myComponent.el.hasAttribute('nul')).toBeFalsy();
+        expect(myComponent.el.hasAttribute('falsy')).toBeTruthy();
+        expect(myComponent.el.hasAttribute('estr')).toBeTruthy();
+        expect(myComponent.el.getAttribute('falsy')).toBe('false');
+        expect(myComponent.el.getAttribute('estr')).toBe('');
+        expect(myComponent.el.getAttribute('zero')).toBe('0');
+
+        myComponent.data.set('nul', '');
+        myComponent.data.set('undef', '');
+        myComponent.nextTick(function () {
+            expect(myComponent.el.hasAttribute('undef')).toBeTruthy();
+            expect(myComponent.el.hasAttribute('nul')).toBeTruthy();
+            expect(myComponent.el.getAttribute('undef')).toBe('');
+            expect(myComponent.el.getAttribute('nul')).toBe('');
+
+            var undef;
+            myComponent.data.set('nul', null);
+            myComponent.data.set('undef', undef);
+
+            myComponent.nextTick(function () {
+                expect(myComponent.el.hasAttribute('undef')).toBeFalsy();
+                expect(myComponent.el.hasAttribute('nul')).toBeFalsy();
+
+                myComponent.dispose();
+                document.body.removeChild(wrap);
+                done();
+            });
+        });
+    });
 });
