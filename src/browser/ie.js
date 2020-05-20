65e6840520f506240c4864cf5e250fa9033540f2
errorrik <errorrik@gmail.com>
2020-05-20 17:27:03 +0800

    fix ie11

--- src/browser/ie.js
+++ src/browser/ie.js
@@ -14,14 +14,17 @@
  * @type {Array}
  */
 var ieVersionMatch = typeof navigator !== 'undefined'
-    && navigator.userAgent.match(/msie\s*([0-9]+)/i);
+    && navigator.userAgent.match(/(msie|trident)(\s*|\/)([0-9]+)/i);
 
 /**
  * ie版本号，非ie时为0
  *
  * @type {number}
  */
-var ie = ieVersionMatch ? /* istanbul ignore next */ ieVersionMatch[1] - 0 : 0;
+var ie = ieVersionMatch ? /* istanbul ignore next */ ieVersionMatch[3] - 0 : 0;
+if (ie && !/msie/i.test(ieVersionMatch[1])) {
+    ie += 4;
+}
 // #[end]
 
 exports = module.exports = ie;
