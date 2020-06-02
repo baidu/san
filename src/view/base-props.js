586e8f2ade119073e974a6f6d975c5f21257261f
errorrik <errorrik@gmail.com>
2020-06-02 17:21:09 +0800

    use splitStr2Obj

--- src/view/base-props.js
+++ src/view/base-props.js
@@ -7,10 +7,13 @@
  * @file 元素的基本属性
  */
 
-var baseProps = {
-    'class': 1,
-    'style': 1,
-    'id': 1
-};
+var splitStr2Obj = require('../util/split-str-2-obj');
+
+/**
+ * 元素的基本属性
+ *
+ * @type {Object}
+ */
+var baseProps = splitStr2Obj('class,style,id');
 
 exports = module.exports = baseProps;
