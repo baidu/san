a4b57e8f3883ba86add211b0f76b51c122ef230a
zhoutingshuai <zhoutingshuai@baidu.com>
2020-08-20 18:31:04 +0800

    fix: keep original when use raw filter on attr, close https://github.com/baidu/san-ssr/issues/69

--- src/parser/integrate-attr.js
+++ src/parser/integrate-attr.js
@@ -154,7 +154,7 @@ function integrateProp(aNode, name, rawValue, options) {
 
             case 1:
                 expr = expr.segs[0];
-                if (expr.type === ExprType.INTERP && expr.filters.length === 0) {
+                if (expr.type === ExprType.INTERP && expr.filters.length === 0 && !expr.original) {
                     expr = expr.expr;
                 }
         }
