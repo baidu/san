6e698ef975d2911f2f25c955ace235eced07f708
errorrik <errorrik@gmail.com>
2021-04-27 00:08:32 +0800

    opti: replace reg by indexOf

--- src/parser/parse-text.js
+++ src/parser/parse-text.js
@@ -12,20 +12,6 @@ var ExprType = require('./expr-type');
 var parseInterp = require('./parse-interp');
 var decodeHTMLEntity = require('../util/decode-html-entity');
 
-/**
- * 对字符串进行可用于new RegExp的字面化
- *
- * @inner
- * @param {string} source 需要字面化的字符串
- * @return {string} 字符串字面化结果
- */
-function regexpLiteral(source) {
-    return source.replace(/[\^\[\]\$\(\)\{\}\?\*\.\+\\]/g, function (c) {
-        return '\\' + c;
-    });
-}
-
-var delimRegCache = {};
 
 /**
  * 解析文本
@@ -37,40 +23,27 @@ var delimRegCache = {};
 function parseText(source, delimiters) {
     delimiters = delimiters || ['{{', '}}'];
 
-    var regCacheKey = delimiters[0] + '>..<' + delimiters[1];
-    var exprStartReg = delimRegCache[regCacheKey];
-    if (!exprStartReg) {
-        exprStartReg = new RegExp(
-            regexpLiteral(delimiters[0])
-                + '\\s*([\\s\\S]+?)\\s*'
-                + regexpLiteral(delimiters[1]),
-            'g'
-        );
-        delimRegCache[regCacheKey] = exprStartReg;
-    }
-
-    var exprMatch;
-
     var walker = new Walker(source);
     var beforeIndex = 0;
 
     var segs = [];
     var original;
 
-    var delimEndLen = delimiters[1].length;
-    while ((exprMatch = walker.match(exprStartReg)) != null) {
-        var interpSource = exprMatch[1];
-        var interpLen = exprMatch[0].length;
-        if (walker.source.slice(walker.index + 1 - delimEndLen, walker.index + 1) === delimiters[1]) {
-            interpSource += walker.source.slice(walker.index, walker.index + 1);
-            walker.index++;
-            interpLen++;
+    var delimStart = delimiters[0];
+    var delimStartLen = delimStart.length;
+    var delimEnd = delimiters[1];
+    var delimEndLen = delimEnd.length;
+    while (1) {
+        var delimStartIndex = walker.source.indexOf(delimStart, walker.index);
+        var delimEndIndex = walker.source.indexOf(delimEnd, walker.index);
+        if (delimStartIndex === -1 || delimEndIndex < delimStartIndex) {
+            break;
         }
 
         // pushStringToSeg
         var strValue = walker.source.slice(
             beforeIndex,
-            walker.index - interpLen
+            delimStartIndex
         );
         strValue && segs.push({
             type: ExprType.STRING,
@@ -78,11 +51,16 @@ function parseText(source, delimiters) {
         });
 
         // pushInterpToSeg
-        var interp = parseInterp(interpSource);
+        if (walker.source.indexOf(delimEnd, delimEndIndex + 1) === delimEndIndex + 1) {
+            delimEndIndex++;
+        }
+        var interp = parseInterp(
+            walker.source.slice(delimStartIndex + delimStartLen, delimEndIndex)
+        );
         original = original || interp.original;
         segs.push(interp);
 
-        beforeIndex = walker.index;
+        beforeIndex = walker.index = delimEndIndex + delimEndLen;
     }
 
     // pushStringToSeg
