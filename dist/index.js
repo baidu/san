0a7084730b9843781cc1f24ed699bb434c60f9cc
errorrik <errorrik@gmail.com>
2021-08-04 14:13:28 +0800

    modify edition which prod env used

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./san.min.js');
+    module.exports = require('./san.js');
 } else {
     module.exports = require('./san.dev.js');
 }
