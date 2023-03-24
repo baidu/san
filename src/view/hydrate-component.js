2d36a2361d7ae0e0b72160562e55ee29edabade9
errorrik <errorrik@gmail.com>
2023-03-24 09:58:22 +0800

    opti querySelectorAll

--- src/view/hydrate-component.js
+++ src/view/hydrate-component.js
@@ -20,31 +20,57 @@ function getTargetComponentClass(path, rootComponentClass) {
 function hydrateComponent(ComponentClass, options) {
     var el = options.el;
 
-    // #[begin] error
     if (!el) {
+        // #[begin] error
         throw new Error('[SAN FATAL] el is required in hydrateComponent.');
-    }
-    // #[end]
-
-    if (el.getAttribute('data-sanssr')  !== 'render-only') {
-        var component = new ComponentClass({
-            el: el
-        });
-
-        return {
-            '': component
-        };
+        // #[end]
+        return {};
     }
 
-    var els = el.querySelectorAll('[data-sanssr-cmpt]');
-    if (el.getAttribute('data-sanssr-cmpt')) {
-        els = Array.prototype.slice.call(els);
-        els.push(el);
+    if (el.getAttribute('data-sanssr') !== 'render-only') {
+        return new ComponentClass(options);
     }
+
+    var stack = [];
+    var stackIndex = -1;
+    var currEl = el;
+    var hydrateRootEls = [];
+
+    while (1) {
+        if (!currEl) {
+            if (stackIndex <= 0) {
+                break;
+            }
+
+            currEl = stack[stackIndex--].nextSibling;
+        }
+        else {
+            switch (currEl.nodeType) {
+                case 1:
+                    if (currEl.hasAttribute('data-sanssr-cmpt')) {
+                        hydrateRootEls.push(currEl);
+                    }
+
+                    var firstChild = currEl.firstChild;
+                    if (firstChild) {
+                        stack[++stackIndex] = currEl;
+                        currEl = firstChild;
+                    }
+                    else {
+                        currEl = currEl.nextSibling;
+                    }
+                    break;
+
+                default:
+                    currEl = currEl.nextSibling;
+            }
+        }
+    }
+
     var index;
     var instances = {};
-    for (index = 0; index < els.length; index++) {
-        var cmptEl = els[index];
+    for (index = 0; index < hydrateRootEls.length; index++) {
+        var cmptEl = hydrateRootEls[index];
         var cmptPath = cmptEl.getAttribute('data-sanssr-cmpt');
         var cmptClass = getTargetComponentClass(cmptPath, ComponentClass);
         var cmpt = new cmptClass({
