5146bf77b336a47a6edb0106ae46cc2b43bea73d
errorrik <errorrik@gmail.com>
2023-12-02 17:39:39 +0800

    fix and add #772 specs

--- test/component-attr-inherit.spec.js
+++ test/component-attr-inherit.spec.js
@@ -27,14 +27,14 @@ describe("Component Attribute Inherit", function () {
         var span = wrap.getElementsByTagName('span')[0];
         expect(span.innerHTML).toContain('Hahaha');
         expect(span.getAttribute('title')).toBe('Hahaha');
-        expect(span.getAttribute('data-title')).toBe('state:Hahaha');
+        expect(span.getAttribute('data-t')).toBe('state:Hahaha');
 
         myComponent.data.set('text', 'Wuwuwu');
 
         myComponent.nextTick(function () {
             expect(span.innerHTML).toContain('Wuwuwu');
             expect(span.getAttribute('title')).toBe('Wuwuwu');
-            expect(span.getAttribute('data-title')).toBe('state:Wuwuwu');
+            expect(span.getAttribute('data-t')).toBe('state:Wuwuwu');
 
             myComponent.dispose();
             document.body.removeChild(wrap);
@@ -76,14 +76,14 @@ describe("Component Attribute Inherit", function () {
         var span = wrap.getElementsByTagName('span')[0];
         expect(span.innerHTML).toContain('Hahaha');
         expect(span.getAttribute('title')).toBe('Hahaha');
-        expect(span.getAttribute('data-title')).toBe('state:Hahaha');
+        expect(span.getAttribute('data-t')).toBe('state:Hahaha');
 
         myComponent.data.set('text', 'Wuwuwu');
 
         myComponent.nextTick(function () {
             expect(span.innerHTML).toContain('Wuwuwu');
             expect(span.getAttribute('title')).toBe('Wuwuwu');
-            expect(span.getAttribute('data-title')).toBe('state:Wuwuwu');
+            expect(span.getAttribute('data-t')).toBe('state:Wuwuwu');
 
             myComponent.dispose();
             document.body.removeChild(wrap);
@@ -96,6 +96,58 @@ describe("Component Attribute Inherit", function () {
             template: '<span><slot/></span>',
             inheritAttrs: false
         });
+
+        var MyComponent = san.defineComponent({
+            template: '<div><ui-inner s-ref="inner" attr-title="{{text}}" attr-data-t="state:{{text}}" class="a" style="display:none" id="happy">{{text}}</ui-inner></div>',
+
+            components: {
+                'ui-inner': Inner
+            }
+        });
+
+        var myComponent = new MyComponent({
+            data: {
+                text: 'Hahaha'
+            }
+        });
+
+        var wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        var innerComponent = myComponent.ref('inner');
+        expect(innerComponent.data.get('$attrs')).toBeUndefined();
+
+        var span = wrap.getElementsByTagName('span')[0];
+        expect(span.innerHTML).toContain('Hahaha');
+        expect(span.hasAttribute('title')).toBeFalsy();
+        expect(span.hasAttribute('data-t')).toBeFalsy();
+        expect(span.className).not.toContain('a');
+        expect(span.id).not.toContain('happy');
+        expect(span.style.display).not.toContain('none');
+
+
+        myComponent.data.set('text', 'Wuwuwu');
+
+        myComponent.nextTick(function () {
+            expect(span.innerHTML).toContain('Wuwuwu');
+            expect(span.hasAttribute('title')).toBeFalsy();
+            expect(span.hasAttribute('data-t')).toBeFalsy();
+            expect(span.className).not.toContain('a');
+            expect(span.id).not.toContain('happy');
+            expect(span.style.display).not.toContain('none');
+
+            myComponent.dispose();
+            document.body.removeChild(wrap);
+            done();
+        });
+    });
+
+    it("inheritAttrs static prop to disable attr inherit, include style/class/id", function (done) {
+        var Inner = san.defineComponent({
+            template: '<span><slot/></span>'
+        });
+        Inner.inheritAttrs = false;
 
         var MyComponent = san.defineComponent({
             template: '<div><ui-inner attr-title="{{text}}" attr-data-t="state:{{text}}" class="a" style="display:none" id="happy">{{text}}</ui-inner></div>',
@@ -118,7 +170,7 @@ describe("Component Attribute Inherit", function () {
         var span = wrap.getElementsByTagName('span')[0];
         expect(span.innerHTML).toContain('Hahaha');
         expect(span.hasAttribute('title')).toBeFalsy();
-        expect(span.hasAttribute('data-title')).toBeFalsy();
+        expect(span.hasAttribute('data-t')).toBeFalsy();
         expect(span.className).not.toContain('a');
         expect(span.id).not.toContain('happy');
         expect(span.style.display).not.toContain('none');
@@ -129,7 +181,7 @@ describe("Component Attribute Inherit", function () {
         myComponent.nextTick(function () {
             expect(span.innerHTML).toContain('Wuwuwu');
             expect(span.hasAttribute('title')).toBeFalsy();
-            expect(span.hasAttribute('data-title')).toBeFalsy();
+            expect(span.hasAttribute('data-t')).toBeFalsy();
             expect(span.className).not.toContain('a');
             expect(span.id).not.toContain('happy');
             expect(span.style.display).not.toContain('none');
@@ -166,14 +218,14 @@ describe("Component Attribute Inherit", function () {
         var span = wrap.getElementsByTagName('span')[0];
         expect(span.innerHTML).toContain('Hahaha');
         expect(span.getAttribute('title')).toBe('nothing');
-        expect(span.getAttribute('data-title')).toBe('state:Hahaha');
+        expect(span.getAttribute('data-t')).toBe('state:Hahaha');
 
         myComponent.data.set('text', 'Wuwuwu');
 
         myComponent.nextTick(function () {
             expect(span.innerHTML).toContain('Wuwuwu');
             expect(span.getAttribute('title')).toBe('nothing');
-            expect(span.getAttribute('data-title')).toBe('state:Wuwuwu');
+            expect(span.getAttribute('data-t')).toBe('state:Wuwuwu');
 
             myComponent.dispose();
             document.body.removeChild(wrap);
@@ -207,25 +259,25 @@ describe("Component Attribute Inherit", function () {
         var innComponent = myComponent.ref('inn');
         var innAttrs = innComponent.data.get('$attrs');
         expect(innAttrs.title).toBe('Hahaha');
-        expect(innAttrs['data-title']).toBe('state:Hahaha');
+        expect(innAttrs['data-t']).toBe('state:Hahaha');
         expect(innComponent.data.get('attrTitle')).toBeUndefined();
 
         var span = wrap.getElementsByTagName('span')[0];
         expect(span.innerHTML).toContain('Hahaha');
         expect(span.getAttribute('title')).toBe('Hahaha');
-        expect(span.getAttribute('data-title')).toBe('state:Hahaha');
+        expect(span.getAttribute('data-t')).toBe('state:Hahaha');
 
         myComponent.data.set('text', 'Wuwuwu');
 
         myComponent.nextTick(function () {
             expect(span.innerHTML).toContain('Wuwuwu');
             expect(span.getAttribute('title')).toBe('Wuwuwu');
-            expect(span.getAttribute('data-title')).toBe('state:Wuwuwu');
+            expect(span.getAttribute('data-t')).toBe('state:Wuwuwu');
 
 
             var innAttrs = innComponent.data.get('$attrs');
             expect(innAttrs.title).toBe('Wuwuwu');
-            expect(innAttrs['data-title']).toBe('state:Wuwuwu');
+            expect(innAttrs['data-t']).toBe('state:Wuwuwu');
 
             expect(innComponent.data.get('attrTitle')).toBeUndefined();
 
@@ -261,14 +313,14 @@ describe("Component Attribute Inherit", function () {
         var span = wrap.getElementsByTagName('span')[0];
         expect(span.innerHTML).toContain('Hahaha');
         expect(span.getAttribute('title')).toBe('Hahaha');
-        expect(span.getAttribute('data-title')).toBe('state:Hahaha');
+        expect(span.getAttribute('data-t')).toBe('state:Hahaha');
 
         myComponent.data.set('text', 'Wuwuwu');
 
         myComponent.nextTick(function () {
             expect(span.innerHTML).toContain('Wuwuwu');
             expect(span.getAttribute('title')).toBe('Wuwuwu');
-            expect(span.getAttribute('data-title')).toBe('state:Wuwuwu');
+            expect(span.getAttribute('data-t')).toBe('state:Wuwuwu');
 
             myComponent.dispose();
             document.body.removeChild(wrap);
