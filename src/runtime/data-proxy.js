2fc150b54585079de9e73cc25e817f09f254e9b8
errorrik <errorrik@gmail.com>
2024-03-23 00:09:51 +0800

    add array method proxy

--- src/runtime/data-proxy.js
+++ src/runtime/data-proxy.js
@@ -4,13 +4,10 @@
  * This source code is licensed under the MIT license.
  * See LICENSE file in the project root for license information.
  *
- * @file 数据类
+ * @file 数据代理
  */
 
 var ExprType = require('../parser/expr-type');
-var evalExpr = require('./eval-expr');
-var DataChangeType = require('./data-change-type');
-var parseExpr = require('../parser/parse-expr');
 
 
 function dataProxy(data) {
@@ -37,43 +34,130 @@ function dataProxy(data) {
             });
         }
 
-        if (proxyWrap.proxy == null) {
-            var paths = basePaths 
-                ? basePaths.concat({
-                    type: ExprType.STRING,
-                    value: prop
-                })
-                : [];
-    
-            var obj = basePaths
-                ? data.get({
-                    type: ExprType.ACCESSOR,
-                    paths: paths
-                })
-                : data.raw;
 
-            proxyWrap.proxy = typeof obj === 'object'
-                ? new Proxy(obj, {
-                    get: function (obj, prop) {
-                        var proxy = getPropProxy(data, paths, prop);
-                        return proxy;
-                    },
-    
-                    set: function (obj, prop, value) {
-                        var expr = {
-                            type: ExprType.ACCESSOR,
-                            paths: paths.concat({
-                                type: ExprType.STRING,
-                                value: prop
-                            })
-                        };
-                        data.set(expr, value);
-                    }
-                })
-                : obj;
+        if (proxyWrap.proxy != null) {
+            return proxyWrap.proxy;
         }
 
-        return proxyWrap.proxy;
+        var paths = basePaths 
+            ? basePaths.concat({type: ExprType.STRING, value: prop})
+            : [];
+
+        var obj = basePaths
+            ? data.get({type: ExprType.ACCESSOR, paths: paths})
+            : data.raw;
+
+        if (obj != null && typeof obj === 'object') {
+            var handlers = {
+                set: function (obj, prop, value) {
+                    var expr = {
+                        type: ExprType.ACCESSOR,
+                        paths: paths.concat({type: ExprType.STRING, value: prop})
+                    };
+                    data.set(expr, value);
+                }
+            };
+
+            handlers.get = obj instanceof Array
+                ? function (arr, prop) {
+                    switch (prop) {
+                        case 'push':
+                            return function () {
+                                var arrLen = arr.length;
+                                var argLen = arguments.length;
+
+                                data.splice(
+                                    {type: ExprType.ACCESSOR, paths: paths}, 
+                                    argLen === 1
+                                        ? [arrLen, 0, arguments[0]]
+                                        : [arrLen, 0].concat(Array.prototype.slice.call(arguments))
+                                );
+            
+                                return arrLen + argLen;
+                            };
+
+                        case 'pop':
+                            return function () {
+                                var arrLen = arr.length;    
+                                if (arrLen) {
+                                    return data.splice(
+                                        {type: ExprType.ACCESSOR, paths: paths}, 
+                                        [arrLen - 1, 1]
+                                    )[0];
+                                }
+                            };
+
+                        case 'shift':
+                            return function () {
+                                return data.splice(
+                                    {type: ExprType.ACCESSOR, paths: paths}, 
+                                    [0, 1]
+                                )[0];
+                            };
+
+                        case 'unshift':
+                            return function () {
+                                var arrLen = arr.length;
+                                var argLen = arguments.length;
+
+                                data.splice(
+                                    {type: ExprType.ACCESSOR, paths: paths}, 
+                                    argLen === 1
+                                        ? [0, 0, arguments[0]]
+                                        : [0, 0].concat(Array.prototype.slice.call(arguments))
+                                );
+            
+                                return arrLen + argLen;
+                            };
+
+                        case 'splice':
+                            return function () {
+                                return data.splice(
+                                    {type: ExprType.ACCESSOR, paths: paths}, 
+                                    Array.prototype.slice.call(arguments)
+                                );
+                            };
+
+                        // case 'remove':
+                        //     return function (value) {
+                        //         var len = arr.length;
+                        //         while (len--) {
+                        //             if (arr[len] === value) {
+                        //                 data.splice(
+                        //                     {type: ExprType.ACCESSOR, paths: paths}, 
+                        //                     [len, 1]
+                        //                 );
+                        //                 break;
+                        //             }
+                        //         }
+                        //     };
+
+                        // case 'removeAt':
+                        //     return function (index) {
+                        //         data.splice(
+                        //             {type: ExprType.ACCESSOR, paths: paths}, 
+                        //             [index, 1]
+                        //         );
+                        //     };
+
+                        case 'length':
+                            return arr.length;
+                    }
+
+                    return getPropProxy(data, paths, prop);
+                }
+                : function (obj, prop) {
+                    return getPropProxy(data, paths, prop);
+                };
+
+            proxyWrap.proxy = new Proxy(obj, handlers);
+            return proxyWrap.proxy;
+        }
+        else {
+            proxyWrap.proxy = obj;
+            return obj;
+        }
+
     }
 }
 
